e064c078444d2acfb1119fc17db653830a89e8ed
Zachary Vacura <zvacura@outlook.com>
2016-09-24 19:38:39 -0500

    Changed travis script to work with 0.8.0

--- test/discord.dart
+++ test/discord.dart
@@ -4,23 +4,23 @@ import 'package:discord_dart/discord.dart' as discord;
 main() {
   var env = Platform.environment;
   var bot = new discord.Client(env['DISCORD_TOKEN']);
-  const duration = const Duration(seconds: 1);
 
   bot.on('ready', (e) async {
-    bot.sendMessage("228308788954791939", "Testing new Travis CI build `#${env['TRAVIS_BUILD_NUMBER']}` from commit `${env['TRAVIS_COMMIT']}` on branch `${env['TRAVIS_BRANCH']}`");
+    var channel = bot.channels['228308788954791939'];
+    channel.sendMessage("Testing new Travis CI build `#${env['TRAVIS_BUILD_NUMBER']}` from commit `${env['TRAVIS_COMMIT']}` on branch `${env['TRAVIS_BRANCH']}`");
 
-    var m = await bot.sendMessage('228308788954791939', "Message test.");
-    await bot.editMessage('228308788954791939', m, "Edit test.");
-    await bot.deleteMessage("228308788954791939", m);
-    await bot.sendMessage("228308788954791939", "--trigger-test");
+    var m = await channel.sendMessage("Message test.");
+    await m.edit("Edit test.");
+    await m.delete();
+    await channel.sendMessage("--trigger-test");
   });
-  
+
   bot.on('message', (e) async {
     var m = e.message;
-    
+
     if (m.channel.id == "228308788954791939" && m.author.id == bot.user.id && m.content == "--trigger-test") {
-      await bot.deleteMessage(m.channel, m);
-      await bot.sendMessage(m.channel, "Tests completed successfully!");
+      await m.delete();
+      await m.channel.sendMessage("Tests completed successfully!");
       print("Discord tests completed successfully!");
       exit(0);
     }
