a488bac8771cdd9da12d9c1c81c1c0e7592bd871
Zachary Vacura <zvacura@outlook.com>
2016-09-27 20:17:17 -0500

    HTTP updates

--- lib/src/http.dart
+++ lib/src/http.dart
@@ -11,28 +11,32 @@ class HTTP {
   /// A map of headers that get sent on each HTTP request.
   Map<String, String> headers;
 
+  /// The HTTP client.
+  http.Client client;
+
   /// Makes a new HTTP manager.
   HTTP(Client client) {
+    this.client = new http.Client();
     this.headers = <String, String>{'User-Agent': 'Discord Dart (https://github.com/Hackzzila/Discord-Dart, ${client.version})', 'Content-Type': 'application/json'};
   }
 
   /// Sends a GET request.
   Future<http.Response> get(String uri) async {
-    return await http.get("${this.host}/$uri", headers: this.headers);
+    return await this.client.get("${this.host}/$uri", headers: this.headers);
   }
 
   /// Sends a POST request.
   Future<http.Response> post(String uri, Object content) async {
-    return await http.post("${this.host}/$uri", body: JSON.encode(content), headers: this.headers);
+    return await this.client.post("${this.host}/$uri", body: JSON.encode(content), headers: this.headers);
   }
 
   /// Sends a PATCH request.
   Future<http.Response> patch(String uri, Object content) async {
-    return await http.patch("${this.host}/$uri", body: JSON.encode(content), headers: this.headers);
+    return await this.client.patch("${this.host}/$uri", body: JSON.encode(content), headers: this.headers);
   }
 
   /// Sends a DELETE request.
   Future<http.Response> delete(String uri) async {
-    return await http.delete("${this.host}/$uri", headers: this.headers);
+    return await this.client.delete("${this.host}/$uri", headers: this.headers);
   }
 }
