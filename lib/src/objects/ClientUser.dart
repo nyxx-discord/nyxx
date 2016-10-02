a4cb9dcf122aea8494f54feb3b8420353d45aa70
Zachary Vacura <zvacura@outlook.com>
2016-10-02 16:10:58 -0500

    Made ClientUser extend User

--- lib/src/objects/ClientUser.dart
+++ lib/src/objects/ClientUser.dart
@@ -1,33 +1,9 @@
 import '../../discord.dart';
 
 /// The client user.
-class ClientUser {
-  /// The client.
-  Client client;
-
-  /// The client user's username.
-  String username;
-
-  /// The client user's ID.
-  String id;
-
-  /// The client user's discriminator.
-  String discriminator;
-
-  /// The client user's avatar hash.
-  String avatar;
-
+class ClientUser extends User {
   /// The client user's email, null if the client user is a bot.
   String email;
-
-  /// The string for mentioning the client user.
-  String mention;
-
-  /// A timestamp for when the client user was created.
-  DateTime createdAt;
-
-  /// Weather or not the client user is a bot.
-  bool bot = false;
 
   /// Weather or not the client user's account is verified.
   bool verified;
@@ -36,25 +12,9 @@ class ClientUser {
   bool mfa;
 
   /// Constructs a new [ClientUser].
-  ClientUser(this.client, Map<String, dynamic> data) {
-    this.username = data['username'];
-    this.id = data['id'];
-    this.discriminator = data['discriminator'];
-    this.avatar = data['avatar'];
-    this.email = data['email'];
-    this.mention = "<@${this.id}>";
-    this.createdAt = this.client.internal.util.getDate(this.id);
-    this.verified = data['verified'];
-    this.mfa = data['mfa_enabled'];
-
-    if (data['bot']) {
-      this.bot = data['bot'];
-    }
-  }
-
-  /// Returns a string representation of this object.
-  @override
-  String toString() {
-    return this.username;
+  ClientUser(Client client, Map<String, dynamic> data) : super(client, data) {
+    this.email = this.map['email'] = data['email'];
+    this.verified = this.map['verified'] = data['verified'];
+    this.mfa = this.map['mfa'] = data['mfa_enabled'];
   }
 }
