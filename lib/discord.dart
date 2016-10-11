a048f67e05626660608015e6703bf790b05ec16e
Hackzzila <admin@hackzzila.com>
2016-10-11 20:36:43 +0000

    Started work on HTTP stuff

--- lib/discord.dart
+++ lib/discord.dart
@@ -3,6 +3,7 @@ library discord;
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:http_utils/http_utils.dart' as http_utils;
 import 'package:http_parser/http_parser.dart' as http_parser;
 import 'package:http/http.dart' as http;
 import 'package:events/events.dart' as events;
