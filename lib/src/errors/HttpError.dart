f941499e7ca79552e1d398e569b498caf22e6c8d
Zachary Vacura <zvacura@outlook.com>
2016-10-13 19:09:31 -0500

    Fixed an error :3

--- lib/src/errors/HttpError.dart
+++ lib/src/errors/HttpError.dart
@@ -11,15 +11,21 @@ class HttpError implements Exception {
   /// Discord's message, if provided.
   int message;
 
-  /// The response body.
-  Map<String, dynamic> body;
+  /// The response body decoded, if it is JSON.
+  Map<String, dynamic> json;
+
+  /// The raw response body.
+  String body;
 
   /// Constructs a new [HttpError].
   HttpError._new(http.Response r) {
-    this.body = JSON.decode(r.body) as Map<String, dynamic>;
+    if (r.headers['content-type'] == "application/json") {
+      this.json = JSON.decode(r.body) as Map<String, dynamic>;
+      this.code = json['code'];
+      this.message = json['message'];
+    }
+    this.body = r.body;
     this.statusCode = r.statusCode;
-    this.code = body['code'];
-    this.message = body['message'];
   }
 
   /// Returns a string representation of this object.
