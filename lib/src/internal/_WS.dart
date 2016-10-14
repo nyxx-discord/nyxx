fc35c88c497d545b0de88b095d0b415523a78516
Zachary Vacura <zvacura@outlook.com>
2016-10-13 19:58:47 -0500

    Autosharding

--- lib/src/internal/_WS.dart
+++ lib/src/internal/_WS.dart
@@ -2,6 +2,8 @@ part of discord;
 
 /// The WS manager for the client.
 class _WS {
+  int shardCount;
+
   /// The base websocket URL.
   String gateway;
 
@@ -10,41 +12,55 @@ class _WS {
 
   /// Makes a new WS manager.
   _WS(this.client) {
-    this.client._http.get('/gateway', true).then((_HttpResponse r) {
+    this.client._http.headers['Authorization'] = "Bot ${client._token}";
+    this.client._http.get("/gateway/bot", true).then((_HttpResponse r) {
       this.gateway = r.json['url'];
-      for (int shardId in this.client._options.shardIds) {
-        Shard shard = new Shard._new(this, shardId);
-        this.client.shards.add(shard);
-
-        shard.onReady.stream.listen((Shard s) {
-          if (!client.ready) {
-            bool match = true;
-            client.guilds.forEach((Guild o) {
-              if (o == null) match = false;
-            });
-
-            bool match2 = true;
-            client.shards.forEach((Shard s) {
-              if (!s.ready) match = false;
-            });
-
-            if (match && match2) {
-              client.ready = true;
-              if (client.user.bot) {
-                client._http
-                    .get('/oauth2/applications/@me', true)
-                    .then((_HttpResponse r) {
-                  client.app = new ClientOAuth2Application._new(client, r.json);
-                  new ReadyEvent._new(client);
-                });
-              } else {
-                new ReadyEvent._new(client);
-              }
-            }
-          }
-        });
+      this.shardCount = r.json['shards'];
+      for (int i = 0; i < shardCount; i++) {
+        setupShard(i);
       }
       this.connectShard(0);
+    }).catchError((err) {
+      this.client._http.get('/gateway', true).then((_HttpResponse r) {
+        this.gateway = r.json['url'];
+        for (int shardId in this.client._options.shardIds) {
+          setupShard(shardId);
+        }
+        this.connectShard(0);
+      });
+    });
+  }
+
+  void setupShard(int shardId) {
+    Shard shard = new Shard._new(this, shardId);
+    this.client.shards.add(shard);
+
+    shard.onReady.stream.listen((Shard s) {
+      if (!client.ready) {
+        bool match = true;
+        client.guilds.forEach((Guild o) {
+          if (o == null) match = false;
+        });
+
+        bool match2 = true;
+        client.shards.forEach((Shard s) {
+          if (!s.ready) match = false;
+        });
+
+        if (match && match2) {
+          client.ready = true;
+          if (client.user.bot) {
+            client._http
+                .get('/oauth2/applications/@me', true)
+                .then((_HttpResponse r) {
+              client.app = new ClientOAuth2Application._new(client, r.json);
+              new ReadyEvent._new(client);
+            });
+          } else {
+            new ReadyEvent._new(client);
+          }
+        }
+      }
     });
   }
 
