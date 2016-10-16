d7856d19620b8c525e2f6d3425287cfda17caf55
Zachary Vacura <zvacura@outlook.com>
2016-10-15 22:14:23 -0500

    Support for discord's latest feature

--- lib/src/objects/Game.dart
+++ lib/src/objects/Game.dart
@@ -1,7 +1,7 @@
 part of discord;
 
 /// A game.
-class Game extends _BaseObj {
+class Game {
   /// The game name.
   String name;
 
@@ -11,9 +11,22 @@ class Game extends _BaseObj {
   /// The game URL, if provided.
   String url;
 
-  Game._new(Client client, Map<String, dynamic> data) : super(client) {
-    this.name = data['name'];
-    this.url = data['url'];
+  /// Makes a new game object.
+  Game(this.name, {this.type: 0, this.url});
+
+  Game._new(Client client, Map<String, dynamic> data) {
+    try {
+      this.name = data['name'].toString();
+    } catch (err) {
+      this.name = null;
+    }
+
+    try {
+      this.url = data['url'].toString();
+    } catch (err) {
+      this.url = null;
+    }
+    
     if (data['type'] is int) {
       this.type = data['type'];
     } else if (data['type'] is String) {
@@ -23,5 +36,13 @@ class Game extends _BaseObj {
         this.type = null;
       }
     }
+  }
+
+  Map<String, dynamic> _toMap() {
+    Map<String, dynamic> map = new Map<String, dynamic>();
+    map['name'] = this.name;
+    map['type'] = this.type;
+    map['url'] = this.url;
+    return map;
   }
 }
