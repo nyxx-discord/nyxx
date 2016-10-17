1ae07d4ea3b568632ceaccdd6ac18253de6cb7f4
Zachary Vacura <zvacura@outlook.com>
2016-10-16 21:09:12 -0500

    dartfmt

--- lib/src/objects/DMChannel.dart
+++ lib/src/objects/DMChannel.dart
@@ -31,7 +31,7 @@ class DMChannel extends Channel {
     if (this._client._options.messageCacheSize > 0) {
       if (this.messages.length >= this._client._options.messageCacheSize) {
         this.messages.values.toList().first._onUpdate.close();
-        this.messages.values.toList().first._onDelete.close();        
+        this.messages.values.toList().first._onDelete.close();
         this.messages.remove(this.messages.values.toList().first.id);
       }
       this.messages[message.id] = message;
--- lib/src/objects/TextChannel.dart
+++ lib/src/objects/TextChannel.dart
@@ -24,7 +24,7 @@ class TextChannel extends GuildChannel {
     if (this._client._options.messageCacheSize > 0) {
       if (this.messages.length >= this._client._options.messageCacheSize) {
         this.messages.values.toList().first._onUpdate.close();
-        this.messages.values.toList().first._onDelete.close(); 
+        this.messages.values.toList().first._onDelete.close();
         this.messages.remove(this.messages.values.toList().first.id);
       }
       this.messages[message.id] = message;
