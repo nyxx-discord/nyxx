4a68c93ec34b442d511d85b185d37682355cb6f0
Zachary Vacura <zvacura@outlook.com>
2016-10-26 08:59:03 -0500

    dartfmt

--- lib/discord_vm.dart
+++ lib/discord_vm.dart
@@ -1,8 +1,7 @@
 library discord.discord_vm;
 
 import 'package:w_transport/w_transport.dart' as w_transport;
-import 'package:w_transport/vm.dart'
-    show vmTransportPlatform;
+import 'package:w_transport/vm.dart' show vmTransportPlatform;
 
 /// Configures the client to run in Dart VM.
 void configureDiscordForVM() {
