26afb74b9bc6307162999b6681940c282a51c607
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-17 13:15:06 +0200

    Added commands framework

---
+++ lib/src/main/Commands.dart
@@ -0,0 +1,40 @@
+part of discord;
+
+class Commands {
+  String _prefix;
+  
+  String get prefix => _prefix;
+
+  List<Command> _commands;
+  
+  Commands(this._prefix) {
+    _commands = [];
+  }
+
+  void dispatch(MessageEvent e) {
+    if(!e.message.author.bot) {
+      if(e.message.content.startsWith('!help'))
+        e.message.channel.sendMessage(content: _createHelp());
+      else if(e.message.content.startsWith(prefix)) {
+       var matched_commands = _commands.where((i) => e.message.content.startsWith((_prefix + i.name)));
+       matched_commands.first.run(e.message);
+      }
+    }
+  }
+
+  String _createHelp() {
+    var buffer = new StringBuffer();
+
+    buffer.writeln("Available commands:");
+
+    _commands.forEach((item) {
+      buffer.writeln("${item.name} - ${item.help}");
+      buffer.writeln("\t Usage: ${item.usage}");
+    });
+  }
+
+  void add(ICommand command) {
+    _commands.add(command);
+    print(_commands);
+  }
+}
---
+++ lib/src/main/commands/Command.dart
@@ -0,0 +1,14 @@
+part of discord;
+
+abstract class Command implements ICommand {
+  String _name;
+  String _help;
+  String _usage;
+  
+  String get name => _name;
+  String get help => _help;
+  String get usage => _usage;
+  
+  Command(this._name, this._help, this._usage);
+
+}
