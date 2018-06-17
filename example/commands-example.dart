114e4206e2da2071afca0f392fd3968bfa8df0f4
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-17 15:08:31 +0200

    Fixed @override

--- example/commands-example.dart
+++ example/commands-example.dart
@@ -24,7 +24,8 @@ void main() {
 class PongCommand extends discord.Command {
   PongCommand() : super("ping", "Checks if bot is connected!", "!ping");
 
-  override run(discord.Message message) {
+  @override
+  run(discord.Message message) {
     message.channel.sendMessage(content: "Pong!");
   }
 }
@@ -32,7 +33,8 @@ class PongCommand extends discord.Command {
 class EchoCommand extends discord.Command {
   EchoCommand() : super("echo", "Echoes bot message!", "!echo <message>");
 
-  override run(discord.Message message) {
+  @override
+  run(discord.Message message) {
     message.channel.sendMessage(content: message.content);
   }
 }
