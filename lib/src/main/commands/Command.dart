7527a8fbbdde6fb8ccc5f38b8a90df83d7352958
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-17 20:51:26 +0200

    Added documentation

--- lib/src/main/commands/Command.dart
+++ lib/src/main/commands/Command.dart
@@ -1,12 +1,23 @@
 part of discord;
 
+/// Absctract class to factory new command
 abstract class Command implements ICommand {
+  /// Name of command. Text which will trigger execution
   String _name;
+
+  /// Help message
   String _help;
+
+  /// Example usage of command
   String _usage;
 
+  /// Name of command. Text which will trigger execution
   String get name => _name;
+
+  /// Help message
   String get help => _help;
+
+  /// Example usage of command
   String get usage => _usage;
 
   Command(this._name, this._help, this._usage);
--- lib/src/main/commands/ICommand.dart
+++ lib/src/main/commands/ICommand.dart
@@ -1,5 +1,7 @@
 part of discord;
 
+/// Command interface to unify.
 abstract class ICommand {
+  /// Function which will be invoked when command triggers
   Future run(Message message);
 }
