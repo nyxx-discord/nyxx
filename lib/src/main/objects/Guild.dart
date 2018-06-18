27b91d60045dba3bd0a053865a82086fe76726ad
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-18 17:38:40 +0200

    Fixes Hackzzila/nyx#20

--- lib/src/main/objects/Guild.dart
+++ lib/src/main/objects/Guild.dart
@@ -132,7 +132,10 @@ class Guild {
 
   /// The guild's icon, represented as URL.
   String iconURL({String format: 'webp', int size: 128}) {
-    return 'https://cdn.${_Constants.host}/icons/${this.id}/${this.icon}.$format?size=$size';
+    if (this.icon == null)
+      return 'https://cdn.${_Constants.host}/icons/${this.id}/${this.icon}.$format?size=$size';
+
+    return null;
   }
 
   /// Returns a string representation of this object.
