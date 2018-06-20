db6411aae08f158874450c6ed109cb3801c3318b
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-20 15:54:57 +0200

    Added CommandFramework test

--- test/travis.sh
+++ test/travis.sh
@@ -32,7 +32,8 @@ echo "PASSED"
 echo ""
 
 if [ "$DISCORD_TOKEN" ]; then
-  dart -c test/discord.dart
+    dart -c test/discord.dart
+    dart -c test/commands.dart
 else
   echo "Discord token not present, skipping Discord tests"
 fi
