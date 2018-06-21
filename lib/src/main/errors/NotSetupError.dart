7bb6e203118029c334ae03f0e16e0a7b49b0b241
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-21 20:33:56 +0200

    Update links to documentation

--- lib/src/main/errors/NotSetupError.dart
+++ lib/src/main/errors/NotSetupError.dart
@@ -1,8 +1,8 @@
 part of discord;
 
 /// Thrown when you don't setup the client first.
-/// See [configureDiscordForBrowser()](https://www.dartdocs.org/documentation/discord/latest/discord.discord_browser/configureDiscordForBrowser.html)
-/// or [configureDiscordForVM()](https://www.dartdocs.org/documentation/discord/latest/discord.discord_vm/configureDiscordForVM.html)
+/// See [configureDiscordForBrowser()](https://www.dartdocs.org/documentation/nyxx/latest/discord.browser/configureDiscordForBrowser.html)
+/// or [configureDiscordForVM()](https://www.dartdocs.org/documentation/nyxx/latest/discord.vm/configureDiscordForVM.html)
 class NotSetupError implements Exception {
   /// Returns a string representation of this object.
   @override
