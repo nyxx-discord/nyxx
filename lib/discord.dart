94654e8251bd871cea5c9248f8c84656ce0e6b22
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-26 13:41:26 +0200

    Removed obsolete entries

--- lib/discord.dart
+++ lib/discord.dart
@@ -11,8 +11,6 @@ part 'src/main/Client.dart';
 
 part 'src/main/Commands.dart';
 part 'src/main/commands/Command.dart';
-part 'src/main/commands/EventHandler.dart';
-part 'src/main/commands/DefaultEventHandler.dart';
 
 part 'src/main/internal/_Constants.dart';
 part 'src/main/internal/_EventController.dart';
