735783d274d1101eed5fae863231dc1eafefd1df
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-26 12:37:36 +0200

    Added Dartdocs

--- lib/src/main/commands/EventHandler.dart
+++ lib/src/main/commands/EventHandler.dart
@@ -1,7 +1,13 @@
 part of discord;
 
+/// Handles dispatch events.
 abstract class EventHandler {
+  /// Invoked when command isn't found in registry
   Future commandNotFound(Message message);
+
+  /// Invoked when command for admins is invoked by non-admin user
   Future forAdminOnly(Message message);
+
+  /// Invoked when command is invoked by user without needed permissions(roles)
   Future requiredPermission(Message message);
 }
