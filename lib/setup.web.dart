23b8ed79b19ea3a73e22af12f8583f1018b5bb36
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-28 18:09:51 +0200

    Fixed imports, introduced new way to setup framework

--- lib/setup.web.dart
+++ lib/setup.web.dart
@@ -1,22 +1,14 @@
 library nyxx.setup;
 
-import 'dart:io' as vm;
+import 'dart:html';
 import 'src/internals.dart' as internals;
 import 'package:w_transport/w_transport.dart' as w_transport;
-import 'package:w_transport/vm.dart' show vmTransportPlatform;
 import 'package:w_transport/browser.dart' show browserTransportPlatform;
-
-/// Configures the client to run in Dart VM.
-void configureDiscordForVM() {
-  w_transport.globalTransportPlatform = vmTransportPlatform;
-  internals.operatingSystem = vm.Platform.operatingSystem;
-  internals.setup = true;
-}
 
 /// Configures the client to run in a browser.
 void configureDiscordForBrowser() {
   w_transport.globalTransportPlatform = browserTransportPlatform;
-  internals.operatingSystem = "generic web browser";
+  internals.operatingSystem = window.navigator.userAgent;
   internals.browser = true;
   internals.setup = true;
 }
