9234e738d9409a69bc3ea02221f75c1fdd30e512
Szymon Uglis <szymon.uglis@tuta.io>
2018-06-30 17:45:39 +0200

    Added docs

--- lib/src/objects/channel/MessageChannel.dart
+++ lib/src/objects/channel/MessageChannel.dart
@@ -1,6 +1,9 @@
 part of nyxx;
 
+/// Provides basic abstraction for sending messages. 
 abstract class MessageChannel {
+
+  /// Sends new message to channel
   Future<Message> send(
       {String content,
       EmbedBuilder embed,
