db511a9032d696b39d678105711f9bc7bdfabf33
Szymon Uglis <szymon.uglis@tuta.io>
2018-07-01 20:44:57 +0200

    Moved UnicodeEmoji to seperated file. Added docs

---
+++ lib/src/objects/message/UnicodeEmoji.dart
@@ -0,0 +1,16 @@
+part of nyxx;
+
+/// Represents unicode emoji. Contains only emoji code.
+class UnicodeEmoji extends Emoji {
+  /// Emoji's unicode code. Hexadecimal number;
+  String code;
+
+  UnicodeEmoji._new(this.code, String name) : super(name);
+  UnicodeEmoji._partial(this.code) : super("");
+
+  /// Encodes Emoji so that can be used in messages.
+  String encode() => new String.fromCharCode(int.parse(code, radix: 16));
+
+  /// Returns encoded string ready to send via message.
+  String toString() => encode();
+}
