821bdef3f351b8e39a5cd53a080a87bef58bca32
Szymon Uglis <szymon.uglis@tuta.io>
2018-07-29 10:41:38 +0200

    Added channel based filtering

--- lib/src/commands/Annotations.dart
+++ lib/src/commands/Annotations.dart
@@ -29,29 +29,44 @@ class AnnotCommand {
   /// Indicated if command is hidden from help
   final bool isHidden;
 
+  /// Indicates if command can be executed in Guild, DM or both.
+  final int guildOnly;
+
   const AnnotCommand(this.cmd,
       [this.isAdmin = false,
       this.requiredRoles = null,
       this.cooldown,
       this.isHidden = false,
-      this.requiredPermissions = null]);
+      this.requiredPermissions = null,
+      this.guildOnly]);
 }
 
 /// Defines new subcommand.
 class Subcommand extends AnnotCommand {
   const Subcommand(String cmd,
-      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions})
-      : super(cmd, isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions);
+      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions, int guildOnly})
+      : super(cmd, isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions, guildOnly);
 }
 
 /// Creates main execution command
 class Maincommand extends AnnotCommand {
   const Maincommand(
-      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions})
-      : super("", isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions);
+      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions, int guildOnly})
+      : super("", isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions, guildOnly);
 }
 
 /// Captures all remaining text into `List<String>`
 class Remainder {
   const Remainder();
+}
+
+class GuildOnly {
+  /// Allows commands to be used only in DM
+  static const int DM = 1 << 0;
+
+  /// Allows commands to be used only in Guild
+  static const int GUILD = 1 << 1;
+
+  /// Allows commands to be used in both DM and Guild
+  static const int BOTH = 1 << 2;
 }
