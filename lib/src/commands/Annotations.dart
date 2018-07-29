617b158e5749b3ad7cc9b0012113c441b4bcfb68
Szymon Uglis <szymon.uglis@tuta.io>
2018-07-29 13:46:22 +0200

    Added checking for nsfw and topics

--- lib/src/commands/Annotations.dart
+++ lib/src/commands/Annotations.dart
@@ -32,27 +32,36 @@ class AnnotCommand {
   /// Indicates if command can be executed in Guild, DM or both.
   final int guildOnly;
 
+  /// If command is nsfw
+  final bool isNsfw;
+
+  /// Topic of command. Can only execute this command if channel has specific topics indicated
+  /// Adding to channel topic `[games, PC]` will allow to only execute commands annotated with this phrases
+  final List<String> topics;
+
   const AnnotCommand(this.cmd,
       [this.isAdmin = false,
       this.requiredRoles = null,
       this.cooldown,
       this.isHidden = false,
       this.requiredPermissions = null,
-      this.guildOnly]);
+      this.guildOnly,
+      this.isNsfw,
+      this.topics]);
 }
 
 /// Defines new subcommand.
 class Subcommand extends AnnotCommand {
   const Subcommand(String cmd,
-      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions, int guildOnly})
-      : super(cmd, isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions, guildOnly);
+      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions, int guildOnly, bool isNsfw, List<String> topics})
+      : super(cmd, isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions, guildOnly, isNsfw, topics);
 }
 
 /// Creates main execution command
 class Maincommand extends AnnotCommand {
   const Maincommand(
-      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions, int guildOnly})
-      : super("", isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions, guildOnly);
+      {bool isAdmin, List<Role> requiredRoles, int cooldown, bool isHidden, List<int> requiredPermissions, int guildOnly, bool isNsfw, List<String> topics})
+      : super("", isAdmin, requiredRoles, cooldown, isHidden, requiredPermissions, guildOnly, isNsfw, topics);
 }
 
 /// Captures all remaining text into `List<String>`
