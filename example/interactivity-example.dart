e1b94d4da3684e3fead8d7d2746b9fd4e729cf19
Szymon Uglis <szymon.uglis@tuta.io>
2018-08-02 21:59:57 +0200

    Reworked interactivity. Paginate has own class

--- example/interactivity-example.dart
+++ example/interactivity-example.dart
@@ -13,11 +13,18 @@ main() async {
     var ch = bot.channels["422285619952222208"] as nyxx.TextChannel;
 
     // Create and send paginated message. After 15 minutes message will be deactivated.
-    await command.Interactivity.paginate(ch, ["SIema", "Czy to dziala?", "Hope so"], timeout: const Duration(minutes: 15));
+    var pagination = new command.Pagination.fromString("Siema siema co tam. "
+        "To bedzie bardzo dlugi paragraf poniewaz potrzebuje duzo tesktu. "
+        "Nie wiem co tu napisac ale mam nadzieje ze nikt tego nie przeczyta "
+        "(a przynajmniej ze zrozumieniem). Jesli jednak przeczytasz to ze "
+        "zrozumieniem to napisz do mnie na priv to powiem ci cos milego. "
+        "Nie no zartowalem ja nie jestem mily.", ch);
+
+    await pagination.paginate(timeout: const Duration(minutes: 15));
 
     // To create poll you need channel, title and map of emojis and Names of options.
     // Result is returned after timeout.
-    var res = await command.Interactivity.createPoll(ch, "Ttul", {
+    var res = await command.createPoll(ch, "Ttul", {
       nyxx.EmojisUnicode.stopwatch: "Stopwatch",
       nyxx.EmojisUnicode.abcd: "abcd"
     }, timeout: const Duration(seconds: 10));
--- lib/src/commands/Interactivity.dart
+++ lib/src/commands/Interactivity.dart
@@ -1,14 +1,76 @@
 part of nyxx.commands;
 
-class Interactivity {
+/// Creates new poll, generates options and collects results. Returns `Map<Emoji, int` as result. [timeout] is set by default to 10 minutes
+Future<Map<Emoji, int>> createPoll(TextChannel channel, String title, Map<Emoji, String> options, {Duration timeout: const Duration(minutes: 10), String message: null, bool delete: false}) async {
+  StringBuffer buffer = new StringBuffer();
+
+  buffer.writeln(title);
+  options.forEach((k, v) {
+    buffer.writeln("${k.format()} - $v");
+  });
+
+  if(message != null)
+  buffer.writeln(message);
+
+  var msg = await channel.send(content: buffer.toString());
+  for (var emoji in options.keys)
+    await msg.createReaction(emoji);
+
+  // Clean memory. Or just null it. Maybe GC will clean this.
+  buffer = null;
+
+  var m = new Map<Emoji, int>();
+  return new Future<Map<Emoji, int>>(() async {
+    await for (var r in msg.onReactionAdded) {
+      if(m.containsKey(r.emoji))
+      m[r.emoji] = m[r.emoji] += 1;
+      else
+      m[r.emoji] = 1;
+    }
+  }).timeout(timeout, onTimeout: () async {
+    if(delete)
+    await msg.delete();
+
+    return m;
+  });
+}
+
+/// Handles pagination interactivity
+class Pagination {
+  List<String> pages;
+
+  MessageChannel channel;
+
+  /// Generates pagination from String. It divides String into 250 char long pages.
+  Pagination.fromString(String str, this.channel) {
+    this.pages = Util.split(str, 250).toList();
+  }
+
+  /// Generates pagination from String but with user specified size of single page.
+  Pagination.fromStringLen(String str, int len, this.channel) {
+    this.pages = Util.split(str, len).toList();
+  }
+
+  /// Generates pagination from String but with user specified number of pages..
+  Pagination.fromStringEq(String str, int pieces, this.channel) {
+    this.pages = Util.splitEqually(str, pieces).toList();
+  }
+
+  /// Generates new pagination from List of Strings. Each list element is single page.
+  Pagination.fromList(this.pages, this.channel);
+
   /// Paginates a list of Strings - each String is different page.
-  static Future<Message> paginate(TextChannel channel, List<String> data, {Duration timeout: const Duration(seconds: 30)}) async {
+  Future<Message> paginate({Duration timeout: const Duration(seconds: 30)}) async {
     var nextEmoji = EmojisUnicode.arrow_forward;
     var backEmoji = EmojisUnicode.arrow_backward;
+    var firstEmoji = EmojisUnicode.track_previous;
+    var lastEmoji = EmojisUnicode.track_next;
 
-    var msg = await channel.send(content: data[0]);
+    var msg = await channel.send(content: pages[0]);
+    await msg.createReaction(firstEmoji);
     await msg.createReaction(backEmoji);
     await msg.createReaction(nextEmoji);
+    await msg.createReaction(lastEmoji);
 
     new Future(() async {
       var currPage = 0;
@@ -20,56 +82,25 @@ class Interactivity {
 
         var emoji = event.emoji as UnicodeEmoji;
         if(emoji.code == nextEmoji.encode()) {
-          if(currPage <= data.length - 2) {
+          if(currPage <= pages.length - 2) {
             ++currPage;
-            await msg.edit(content: data[currPage]);
+            await msg.edit(content: pages[currPage]);
           }
         } else if (emoji.code == backEmoji.encode()) {
           if(currPage >= 1) {
             --currPage;
-            await msg.edit(content: data[currPage]);
+            await msg.edit(content: pages[currPage]);
           }
+        } else if (emoji.code == firstEmoji.encode()) {
+            await msg.edit(content: pages.first);
+            currPage = 0;
+        } else if (emoji.code == lastEmoji.encode()) {
+          await msg.edit(content: pages.last);
+          currPage = pages.length;
         }
-
-        print(currPage);
       }
     }).timeout(timeout);
 
     return msg;
-  }
-
-  /// Creates new poll, generates options and collects results. Returns `Map<Emoji, int` as result. [timeout] is set by default to 10 minutes
-  static Future<Map<Emoji, int>> createPoll(TextChannel channel, String title, Map<Emoji, String> options, {Duration timeout: const Duration(minutes: 10), String message: null, bool delete: false}) async {
-    StringBuffer buffer = new StringBuffer();
-
-    buffer.writeln(title);
-    options.forEach((k, v) {
-      buffer.writeln("${k.format()} - $v");
-    });
-
-    if(message != null)
-      buffer.writeln(message);
-
-    var msg = await channel.send(content: buffer.toString());
-    for (var emoji in options.keys)
-      await msg.createReaction(emoji);
-
-    // Clean memory. Or just null it. Maybe GC will clean this.
-    buffer = null;
-
-    var m = new Map<Emoji, int>();
-    return new Future<Map<Emoji, int>>(() async {
-      await for (var r in msg.onReactionAdded) {
-        if(m.containsKey(r.emoji))
-          m[r.emoji] = m[r.emoji] += 1;
-        else
-          m[r.emoji] = 1;
-      }
-    }).timeout(timeout, onTimeout: () async {
-      if(delete)
-        await msg.delete();
-
-      return m;
-    });
   }
 }
