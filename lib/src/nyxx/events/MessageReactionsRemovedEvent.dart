eb37e1a36a4ac53f6daa8b093c5645e5122cf63c
Szymon Uglis <szymon.uglis@tuta.io>
2018-08-08 12:19:32 +0200

    Fixed deserializing

--- lib/src/nyxx/events/MessageReactionsRemovedEvent.dart
+++ lib/src/nyxx/events/MessageReactionsRemovedEvent.dart
@@ -6,7 +6,7 @@ class MessageReactionsRemovedEvent {
 
   MessageReactionsRemovedEvent._new(Client client, Map<String, dynamic> json) {
     this.channel = client.channels[new Snowflake(json['d']['channel_id'] as String)] as MessageChannel;
-    this.message = channel.getMessage(new Snowflake(json['id']['message_id'] as String));
+    this.message = channel.getMessage(new Snowflake(json['d']['message_id'] as String));
 
     client._events.onMessageReactionsRemoved.add(this);
   }
