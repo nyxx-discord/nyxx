f987e331984e7045e216c4b8202b51f00c47c179
Szymon Uglis <szymon.uglis@tuta.io>
2018-08-10 03:38:28 +0200

    Fixed date deserializing

--- lib/src/nyxx/events/ChannelPinsUpdateEvent.dart
+++ lib/src/nyxx/events/ChannelPinsUpdateEvent.dart
@@ -10,8 +10,7 @@ class ChannelPinsUpdateEvent {
 
   ChannelPinsUpdateEvent._new(Client client, Map<String, dynamic> json) {
     this.channelId = new Snowflake((json['d']['channel_id'] as String));
-    this.lastPingTimestamp = new DateTime.fromMillisecondsSinceEpoch(
-        (json['d']['last_pin_timestamp'] as int));
+    this.lastPingTimestamp = DateTime.parse(json['d']['last_pin_timestamp'] as String);
     client._events.onChannelPinsUpdate.add(this);
   }
 }
