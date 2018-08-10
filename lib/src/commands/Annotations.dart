7db68c41d1c7f2aab193d1c8c935fe4594d00044
Szymon Uglis <szymon.uglis@tuta.io>
2018-08-10 03:41:00 +0200

    Added docs

--- lib/src/commands/Annotations.dart
+++ lib/src/commands/Annotations.dart
@@ -1,5 +1,29 @@
 part of nyxx.commands;
 
+/// Defines command. Can be placed above method or class.
+/// If places above class this means that class is group of commands and all
+/// commans will be have prefix with given name.
+/// Placed on method it defines command handler for specified command.
+///
+/// Code above cretaes new command group with name `cmd` and subcommand `check`.
+/// Group also has default command handler if only `cmd` invoked
+/// ```
+/// @Command(name: "cmd")
+/// class ExampleCommand {
+///   // Invoked when message == `!cmd`
+///   @Command(main: true)
+///   Future main() async {
+///    await reply(content: "Some reply");
+///   }
+///
+///   // This is subcommand and will be invoked when message
+///   // is equal with `!cmd check`
+///   @Command(name: "check")
+///   Future main() async {
+///     await reply(content: "Checked!");
+///   }
+/// }
+/// ```
 class Command {
   /// Name of command. Text which will trigger execution
   final String name;
@@ -13,8 +37,10 @@ class Command {
   const Command({this.name, this.aliases, this.main});
 }
 
+/// Defines additional properties which will restrict user access to command
+/// All fields are optional and wont affect command execution of not set.
 class Cons {
-  /// Indicates if commands is restricted to admins
+  /// Checks if user is in admins list provided at creating CommandsFramework
   final bool isAdmin;
 
   /// List of roles required to execute command
@@ -26,13 +52,10 @@ class Cons {
   /// Cooldown for command in seconds
   final int cooldown;
 
-  /// Indicated if command is hidden from help
-  final bool isHidden;
-
-  /// Indicates if command can be executed in Guild, DM or both.
+  /// Allows to restrict command to be used only on guild or only in DM or both
   final int guildOnly;
 
-  /// If command is nsfw
+  /// If command is nfsw it wont be invoked in no nsfw channels
   final bool isNsfw;
 
   /// Topic of command. Can only execute this command if channel has specific topics indicated
@@ -43,18 +66,18 @@ class Cons {
       {this.isAdmin = false,
       this.requiredRoles,
       this.cooldown,
-      this.isHidden = false,
       this.requiredPermissions,
       this.guildOnly,
       this.isNsfw,
       this.topics});
 }
 
-/// Captures all remaining text into `List<String>`
+/// Captures all remaining text into `List<String>` or `String`
 class Remainder {
   const Remainder();
 }
 
+/// Allows to specify destination of command.
 class GuildOnly {
   /// Allows commands to be used only in DM
   static const int DM = 1 << 0;
