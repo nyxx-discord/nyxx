a070bb7d3f11176d176b9c6b309daaa6b3cb2f3a
Szymon Uglis <szymon.uglis@tuta.io>
2018-08-29 21:54:16 +0200

    Simplified tests

--- test/travis.dart
+++ test/travis.dart
@@ -4,12 +4,24 @@ import 'dart:async';
 import 'package:nyxx/nyxx.dart' as nyxx;
 import 'package:nyxx/commands.dart' as command;
 
+// Messages on which we delete message
+var ddel = [
+  "--trigger-test",
+  "test is working correctly",
+  "test is working correctly",
+  "Command '~~notFound' not found!",
+  "Command is on cooldown!. Wait a few seconds!",
+  "14 Example data",
+];
+
+// Example service
 class StringService extends command.Service {
   String data = "Example data";
 
   StringService();
 }
 
+// Somme commands to test CommandsFramework behaviour
 @command.Command(name: "test")
 class TestCommand extends command.CommandContext {
   @command.Command(main: true)
@@ -31,6 +43,16 @@ class CooldownCommand extends command.CommandContext {
   run() async {}
 }
 
+// -------------------------------------------------------
+
+nyxx.EmbedBuilder createTestEmbed() {
+  return new nyxx.EmbedBuilder()
+    ..title = "Test title"
+    ..addField(name: "Test field", value: "Test value");
+}
+
+// -------------------------------------------------------
+
 void main() {
   var env = Platform.environment;
   var bot = new nyxx.Client(env['DISCORD_TOKEN'], ignoreExceptions: false);
@@ -50,12 +72,6 @@ void main() {
     print('Timed out waiting for messages');
     exit(1);
   });
-
-  nyxx.EmbedBuilder createTestEmbed() {
-    return new nyxx.EmbedBuilder()
-      ..title = "Test title"
-      ..addField(name: "Test field", value: "Test value");
-  }
 
   bot.onReady.listen((e) async {
     var channel =
@@ -97,50 +113,24 @@ void main() {
   bot.onMessage.listen((e) async {
     var m = e.message;
 
-    if (m.channel.id == "422285619952222208" &&
-        m.author.id == bot.user.id &&
-        m.content == "--trigger-test") {
-      await m.delete();
-    }
+    if(m.channel.id != "422285619952222208" &&
+        m.author.id != bot.user.id)
+        return;
 
-    if (m.channel.id == "422285619952222208" &&
-        m.author.id == bot.user.id &&
-        m.content == "test is working correctly") {
+    if(ddel.contains(m.content))
       await m.delete();
-    }
 
-    if (m.channel.id == "422285619952222208" &&
-        m.author.id == bot.user.id &&
-        m.content == "Command '~~notFound' not found!") {
-      await m.delete();
-    }
-
-    if (m.channel.id == "422285619952222208" &&
-        m.author.id == bot.user.id &&
-        m.content == "Command is on cooldown!. Wait a few seconds!") {
-      await m.delete();
-    }
-
-    if (m.channel.id == "422285619952222208" &&
-        m.author.id == bot.user.id &&
-        m.content == "14 Example data") {
-      await m.delete();
-    }
-
-    if (m.channel.id == "422285619952222208" &&
-        m.author.id == bot.user.id &&
-        m.content == "Testing embed!") {
+    if (m.content == "Testing embed!") {
       if (m.embeds.length > 0) {
-        var embed = m.embeds.values.toList()[0];
+        var embed = m.embeds.values.first;
         if (embed.title == "Test title" && embed.fields.length > 0) {
-          var field = embed.fields.values.toList()[0];
+          var field = embed.fields.values.first;
 
           if (field.name == "Test field" &&
               field.content == "Test value" &&
               !field.inline) {
             await m.channel.send(content: "Tests completed successfully!");
             print("Nyxx tests completed successfully!");
-            //await bot.destroy();
             exit(0);
           }
         }
