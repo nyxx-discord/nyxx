d0c7dff6c036a111da57afa50e35ee64d1597235
Szymon Uglis <szymon.uglis@tuta.io>
2018-09-02 20:49:58 +0200

    Added docs, allowed to create custom poll messages

--- lib/src/commands/Interactivity.dart
+++ lib/src/commands/Interactivity.dart
@@ -1,25 +1,43 @@
 part of nyxx.commands;
 
 /// Creates new poll, generates options and collects results. Returns `Map<Emoji, int` as result. [timeout] is set by default to 10 minutes
+/// 
+/// ```
+/// Future<void> examplePoll(CommandContext ctx) async {
+///   var results = createPoll(ctx.channel, "This is awesome poll", {UnicodeEmoji(""): "One option", UnicodeEmoji(""): "second option"});
+/// }
+/// ```
 Future<Map<Emoji, int>> createPoll(
     TextChannel channel, String title, Map<Emoji, String> options,
     {Duration timeout = const Duration(minutes: 10),
     String message,
-    bool delete = false}) async {
-  StringBuffer buffer = StringBuffer();
+    bool delete = false,
+    Object Function(Map<Emoji, String> options, String message) messageFactory}) async {
+  var toSend;
 
-  buffer.writeln(title);
-  options.forEach((k, v) {
-    buffer.writeln("${k.format()} - $v");
-  });
+  if(messageFactory == null) {
+    StringBuffer buffer = StringBuffer();
 
-  if (message != null) buffer.writeln(message);
+    buffer.writeln(title);
+    options.forEach((k, v) {
+      buffer.writeln("${k.format()} - $v");
+    });
 
-  var msg = await channel.send(content: buffer.toString());
+    if (message != null) buffer.writeln(message);
+
+    toSend = buffer.toString();
+  } else {
+    toSend = messageFactory(options, message);
+  }
+
+  Message msg;
+  if(toSend is String)
+    msg = await channel.send(content: toSend);
+  else if (toSend is EmbedBuilder)
+    msg = await channel.send(embed: toSend);
+  else return null;
+
   for (var emoji in options.keys) await msg.createReaction(emoji);
-
-  // Clean memory. Or just null it. Maybe GC will clean this.
-  buffer = null;
 
   var m = Map<Emoji, int>();
   return Future<Map<Emoji, int>>(() async {
@@ -36,29 +54,40 @@ Future<Map<Emoji, int>> createPoll(
   });
 }
 
-/// Handles pagination interactivity
+/// Handles pagination interactivity. Allows to create pagnated messages from List<String> 
+/// Factory constructors allows to create message from String directly.
+/// 
+/// Pagination is sent by [paginate] method. And returns [Message] instance of sent message. 
+/// 
+/// ```
+/// var pagination = new Pagination(["This is simple paginated", "data. Use it if you", "want to partition text by yourself"], ctx,channel);
+/// // It generated 2 equal (possibly) pages.
+/// var paginatedMessage = new Pagination.fromStringEq("This is text for pagination", 2);
+/// ```
 class Pagination {
+  /// Pages of paginated message
   List<String> pages;
 
+  /// Channel where message will be sent
   MessageChannel channel;
 
+  /// Generates new pagination from List of Strings. Each list element is single page.
+  Pagination(this.pages, this.channel);
+
   /// Generates pagination from String. It divides String into 250 char long pages.
-  Pagination.fromString(String str, this.channel) {
-    this.pages = util.split(str, 250).toList();
+  factory Pagination.fromString(String str, MessageChannel channel) {
+    return new Pagination(util.split(str, 250).toList(), channel);
   }
 
   /// Generates pagination from String but with user specified size of single page.
-  Pagination.fromStringLen(String str, int len, this.channel) {
-    this.pages = util.split(str, len).toList();
+  factory Pagination.fromStringLen(String str, int len, MessageChannel channel) {
+    return new Pagination(util.split(str, len).toList(), channel);
   }
 
   /// Generates pagination from String but with user specified number of pages.
-  Pagination.fromStringEq(String str, int pieces, this.channel) {
-    this.pages = util.splitEqually(str, pieces).toList();
+  factory Pagination.fromStringEq(String str, int pieces, MessageChannel channel) {
+    return new Pagination(util.splitEqually(str, pieces).toList(), channel);
   }
-
-  /// Generates new pagination from List of Strings. Each list element is single page.
-  Pagination.fromList(this.pages, this.channel);
 
   /// Paginates a list of Strings - each String is different page.
   Future<Message> paginate(
