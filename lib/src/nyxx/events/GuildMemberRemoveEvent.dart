525970e0c9cc999fcf90960171e494a1fe6930ef
Szymon Uglis <szymon.uglis@tuta.io>
2018-09-07 20:37:02 +0200

    Fixes #7 #8

--- lib/src/nyxx/events/GuildMemberRemoveEvent.dart
+++ lib/src/nyxx/events/GuildMemberRemoveEvent.dart
@@ -11,11 +11,14 @@ class GuildMemberRemoveEvent {
   GuildMemberRemoveEvent._new(Nyxx client, Map<String, dynamic> json) {
     if (client.ready && json['d']['user']['id'] != client.user.id) {
       this.guild = client.guilds[Snowflake(json['d']['guild_id'] as String)];
-      guild.memberCount--;
-      this.user =
-          Member._new(client, json['d']['user'] as Map<String, dynamic>);
-      guild.members.remove(user.id);
-      client._events.onGuildMemberRemove.add(this);
+
+      if(guild != null) {
+        guild.memberCount--;
+        this.user =
+            Member._new(client, json['d']['user'] as Map<String, dynamic>);
+        guild.members.remove(user.id);
+        client._events.onGuildMemberRemove.add(this);
+      }
     }
   }
 }
