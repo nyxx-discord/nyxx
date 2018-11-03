0647008293d217c07e07bb4407d339caa7a81f6a
Benny Jacobs <Benny@gmx.it>
2018-11-03 19:15:23 +0100

    Correct function calls in NotSetupError documentation

--- lib/src/nyxx/errors/NotSetupError.dart
+++ lib/src/nyxx/errors/NotSetupError.dart
@@ -1,8 +1,8 @@
 part of nyxx;
 
 /// Thrown when you don't setup the client first.
-/// See configureDiscordForBrowser()
-/// or configureDiscordForVM()
+/// See configureNyxxForBrowser()
+/// or configureNyxxForVM()
 class NotSetupError implements Exception {
   /// Returns a string representation of this object.
   @override
