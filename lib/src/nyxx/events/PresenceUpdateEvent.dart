c5b558c727de70d6ebf5733543826ca2b0d1df9a
Szymon Uglis <szymon.uglis@tuta.io>
2019-03-03 23:03:56 +0100

    Don't invoke event when there is no such guild

--- lib/src/nyxx/events/PresenceUpdateEvent.dart
+++ lib/src/nyxx/events/PresenceUpdateEvent.dart
@@ -11,6 +11,9 @@ class PresenceUpdateEvent {
   PresenceUpdateEvent._new(Map<String, dynamic> json, Nyxx client) {
     var guild = client.guilds[Snowflake(json['d']['guild_id'] as String)];
 
+    if(guild == null)
+      return;
+
     if (guild != null)
       this.member = guild.members[Snowflake(json['d']['user']['id'] as String)];
 
