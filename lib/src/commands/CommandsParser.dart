da194bd95d5c45e84eee21ea043b3f34d5bd60d9
Szymon Uglis <szymon.uglis@tuta.io>
2019-07-16 01:41:09 +0200

    Added abillity to specify predicate in CommandParser

--- lib/src/commands/CommandsParser.dart
+++ lib/src/commands/CommandsParser.dart
@@ -3,6 +3,8 @@ part of nyxx.commands;
 typedef Future<void> CommandHandler(
     Message message, Member author, MessageChannel channel, List<String> args);
 
+typedef FutureOr<bool> Predicate(Message message, Member author, MessageChannel channel);
+
 bool _isCommandMatching(List<String> command, List<String> message) {
   if(message.length < command.length)
     return false;
@@ -20,12 +22,19 @@ class CommandParser {
 
   String prefix;
 
-  CommandParser(this.prefix, Nyxx client) {
+  Predicate _predicate;
+
+  CommandParser(this.prefix, Nyxx client, [this._predicate]) {
     _commands = Map();
 
     client.onReady.listen((_) {
-      client.onMessageReceived.listen((event) {
+      client.onMessageReceived.listen((event) async {
         if (!event.message.content.startsWith(prefix)) return;
+
+
+        if(_predicate != null && await _predicate(event.message, event.message.guild != null ? event.message.guild.members[event.message.author.id] : null, event.message.channel)) {
+          return;
+        }
 
         var trimmedContent = event.message.content.replaceFirst(prefix, "");
         var splittedContent = trimmedContent.split(" ");
