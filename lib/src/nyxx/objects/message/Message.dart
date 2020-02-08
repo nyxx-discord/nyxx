ade16b3f9b07ef848797cfbd4edfb66f1221c63e
One-Nub <38899321+One-Nub@users.noreply.github.com>
2020-02-08 00:00:46 -0600

    An attempt at a fix for a NoSuchMethodError (and running dartfmt)
    The same changes apply on the rewrite_modular branch too; other than 
    dartfmt changes

--- lib/src/nyxx/objects/message/Message.dart
+++ lib/src/nyxx/objects/message/Message.dart
@@ -50,8 +50,7 @@ class Message extends SnowflakeEntity implements GuildEntity, Disposable {
   MessageType type;
 
   /// Returns clickable url to this message.
-  String get url =>
-      "https://discordapp.com/channels/${this.guild.id}"
+  String get url => "https://discordapp.com/channels/${this.guild.id}"
       "/${this.channel.id}/${this.id}";
 
   Message._new(Map<String, dynamic> raw, this.client)
@@ -96,9 +95,10 @@ class Message extends SnowflakeEntity implements GuildEntity, Disposable {
         });
       }
     } else {
-      this.author = client.users[Snowflake(raw['author']['id'] as String)];
+      if (raw['author'] != null)
+        this.author = client.users[Snowflake(raw['author']['id'] as String)];
 
-      if (this.author == null) {
+      if (this.author == null && raw['member'] != null) {
         var r = raw['author'];
         r['member'] = raw['member'];
         var author = _ReverseMember(r as Map<String, dynamic>,
@@ -178,7 +178,8 @@ class Message extends SnowflakeEntity implements GuildEntity, Disposable {
       EmbedBuilder embed,
       bool tts = false,
       bool disableEveryone}) async {
-    if (this.author.id != client.self.id) return Future.error("Cannot edit someones message");
+    if (this.author.id != client.self.id)
+      return Future.error("Cannot edit someones message");
 
     var body = Map<String, dynamic>();
     if (content != null)
@@ -234,11 +235,11 @@ class Message extends SnowflakeEntity implements GuildEntity, Disposable {
     return client._http.send('DELETE', "/channels/${channel.id}/pins/$id");
   }
 
-
   @override
   bool operator ==(other) {
-    if(other is Message) {
-      return other.content == this.content || other.embeds.any((e) => this.embeds.any((f) => e == f));
+    if (other is Message) {
+      return other.content == this.content ||
+          other.embeds.any((e) => this.embeds.any((f) => e == f));
     }
 
     return false;
@@ -253,6 +254,4 @@ class Message extends SnowflakeEntity implements GuildEntity, Disposable {
   }
 }
 
-class MessageType {
-
-}
+class MessageType {}
