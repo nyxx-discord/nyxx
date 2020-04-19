8247e4568cd165c26d69cc01d966306ddf69f29a
Szymon Uglis <szymon.uglis@tuta.io>
2020-04-19 19:46:20 +0200

    Added tests for commander. Added support for commons handlers before and after executing command [ci skip]

--- nyxx.commander/test/commander-test.dart
+++ nyxx.commander/test/commander-test.dart
@@ -1,13 +1,72 @@
+import 'dart:async';
 import 'dart:io';
 
-import 'package:nyxx.commander/commander.dart';
 import 'package:nyxx/Vm.dart';
+import 'package:nyxx.commander/commander.dart';
 
 void main() {
   setupDefaultLogging();
-  final bot = NyxxVm(Platform.environment['DISCORD_TOKEN'], ignoreExceptions: true);
-  final commander = Commander(bot, prefix: "s!!!")
-    ..registerCommand("test", (context, message) async {
-      context.channel.send(content: "TEST!");
-    });
+
+  var env = Platform.environment;
+  var bot = NyxxVm(env['DISCORD_TOKEN'], ignoreExceptions: false);
+
+  bot.onMessageReceived.listen((event) async {
+    if(event.message == null) {
+      return;
+    }
+
+    if(event.message!.content == "Test 1") {
+      event.message!.delete();
+    }
+
+    if(event.message!.content == "Test 2") {
+      event.message!.delete();
+    }
+
+    if(event.message!.content == "Test 10") {
+      await event.message!.delete();
+
+      await event.message!.channel.send(content: "Commander tests completed sucessfuly!");
+      exit(0);
+    }
+  });
+
+  bot.onReady.listen((e) async {
+    var channel = bot.channels[Snowflake('422285619952222208')] as TextChannel;
+
+    await channel.send(content: "Testing Commander");
+
+    var msg1 = await channel.send(content: "test>test1");
+    msg1.delete();
+
+    var msg2 = await channel.send(content: "test>test2 arg1");
+    msg2.delete();
+
+    var msg3 = await channel.send(content: "test>test3");
+    msg3.delete();
+  });
+
+  Commander(bot, prefix: "test>", beforeCommandHandler: (context, message)  async{
+    if(message.endsWith("test3")) {
+      await context.channel.send(content: "Test 10");
+      return true;
+    }
+
+    return true;
+  })..registerCommand("test1", (context, message) async {
+    await context.channel.send(content: "Test 1");
+  })..registerCommand("test2", (context, message) async {
+    var args = message.split(" ");
+
+    if(args.length == 2 && args.last == "arg1") {
+      await context.channel.send(content: "Test 2");
+    }
+  })..registerCommand("test3", (context, message) async {
+    await context.message.delete();
+  });
+
+  Timer(const Duration(seconds: 60), () {
+    print('Timed out waiting for messages');
+    exit(1);
+  });
 }
