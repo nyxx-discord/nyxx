779e2d83d497ae97e1379978d36b49a1caacf478
Szymon Uglis <szymon.uglis@tuta.io>
2020-04-20 15:34:33 +0200

    Deprecated nyxx.commands [ci skip]

--- nyxx.commands/lib/src/TypeConverter.dart
+++
@@ -1,8 +0,0 @@
-part of nyxx.commands;
-
-/// Interface for creating TypeConverter.
-/// Overriding this class allows to later register it into [CommandsFramework]
-abstract class TypeConverter<T> {
-  Future<T> parse(String from, Message msg);
-  Type get _type => T;
-}
