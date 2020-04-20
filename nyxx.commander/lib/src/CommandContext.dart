5d0b799ee851b2faf0758825c71cf4402c21a05e
Szymon Uglis <szymon.uglis@tuta.io>
2020-04-20 21:11:02 +0200

    Added interactivity methods to context [ci skip]

--- nyxx.commander/lib/src/CommandContext.dart
+++ nyxx.commander/lib/src/CommandContext.dart
@@ -18,4 +18,155 @@ class CommandContext {
   Member? get member => guild?.members[author!.id];
 
   CommandContext._new(this.channel, this.author, this.guild, this.message);
+
+  /// Reply to message. It allows to send regular message, Embed or both.
+  ///
+  /// ```
+  /// /// Class body
+  /// @Command()
+  /// Future<void> getAv(CommandContext context, String message) async {
+  ///   await context.reply(content: uset.avatarURL());
+  /// }
+  /// ```
+  Future<Message> reply(
+      {dynamic content,
+        List<AttachmentBuilder>? files,
+        EmbedBuilder? embed,
+        bool? tts,
+        AllowedMentions? allowedMentions,
+        MessageBuilder? builder}) {
+    return channel.send(
+        content: content,
+        embed: embed,
+        tts: tts,
+        files: files,
+        builder: builder,
+        allowedMentions: allowedMentions);
+  }
+
+  /// Reply to messages, then delete it when [duration] expires.
+  ///
+  /// ```
+  /// @Command()
+  /// Future<void> getAv(CommandContext context, String message) async {
+  ///   await context.replyTemp(content: user.avatarURL());
+  /// }
+  /// ```
+  Future<Message> replyTemp(Duration duration,
+      {dynamic content,
+        List<AttachmentBuilder>? files,
+        EmbedBuilder? embed,
+        bool? tts,
+        AllowedMentions? allowedMentions,
+        MessageBuilder? builder}) {
+    return channel.send(
+        content: content,
+        embed: embed,
+        files: files,
+        tts: tts,
+        builder: builder,
+        allowedMentions: allowedMentions).then((msg) {
+      Timer(duration, () => msg.delete());
+      return msg;
+    });
+  }
+
+  /// Replies to message after delay specified with [duration]
+  /// ```
+  /// @Command()
+  /// Future<void> getAv(CommandContext context, String message) async {
+  ///   await context.replyDelayed(Duration(seconds: 2), content: user.avatarURL());
+  /// }
+  /// ```
+  Future<Message> replyDelayed(Duration duration,
+      {dynamic content,
+        List<AttachmentBuilder>? files,
+        EmbedBuilder? embed,
+        bool? tts,
+        AllowedMentions? allowedMentions,
+        MessageBuilder? builder}) {
+    return Future.delayed(
+        duration,
+            () => channel.send(
+            content: content,
+            embed: embed,
+            files: files,
+            tts: tts,
+            builder: builder,
+            allowedMentions: allowedMentions));
+  }
+
+  /// Gather emojis of message in given time
+  ///
+  /// ```
+  /// Future<void> getAv(CommandContext context, String message) async {
+  ///   var msg = await context.replyDelayed(content: context.user.avatarURL());
+  ///   var emojis = await context.collectEmojis(msg, Duration(seconds: 15));
+  ///
+  ///   ...
+  /// }
+  /// ```
+  Future<Map<Emoji, int>?> collectEmojis(Message msg, Duration duration) {
+    return Future<Map<Emoji, int>>(() async {
+      var m = Map<Emoji, int>();
+
+      await for (var r in msg.client.onMessageReactionAdded.where((evnt) => evnt.message != null && evnt.message!.id == msg.id)) {
+        if (m.containsKey(r.emoji))
+          m[r.emoji] += 1;
+        else
+          m[r.emoji] = 1;
+      }
+
+      return m;
+    }).timeout(duration, onTimeout: () => null);
+  }
+
+  /// Waits for first [TypingEvent] and returns it. If timed out returns null.
+  /// Can listen to specific user by specifying [user]
+  Future<TypingEvent?> waitForTyping(User user,
+      {Duration timeout = const Duration(seconds: 30)}) {
+    return user.client.onTyping
+        .firstWhere((e) => e.user == user && e.channel == this.channel)
+        .timeout(timeout, onTimeout: () => null);
+  }
+
+  /// Gets all context channel messages that satisfies [predicate].
+  ///
+  /// ```
+  /// @Command()
+  /// Future<void> getAv(CommandContext context, String message) async {
+  ///   var messages = await context.nextMessagesWhere((msg) => msg.content.startsWith("fuck"));
+  /// }
+  /// ```
+  Stream<MessageReceivedEvent> nextMessagesWhere(bool predicate(MessageReceivedEvent msg), {int limit = 100}) {
+    return channel.onMessage.where(predicate).take(limit);
+  }
+
+  /// Gets next [num] number of any messages sent within one context (same channel).
+  ///
+  /// ```
+  /// @Command()
+  /// Future<void> getAv(CommandContext context, String message) async {
+  ///   // gets next 10 messages
+  ///   var messages = await context.nextMessages(10);
+  /// }
+  /// ```
+  Stream<MessageReceivedEvent> nextMessages(int num) {
+    return channel.onMessage.take(num);
+  }
+
+  /// Returns stream of all code blocks in message
+  /// For now it only parses codeblock which starts in first line
+  /// Language string `dart, java` will be ignored and not included
+  /// """
+  /// n> eval ```(dart)?
+  ///   await reply(content: 'no to elo');
+  /// ```
+  /// """
+  Stream<String> getCodeBlocks() async* {
+    var regex = RegExp(r"```(\w+)?(\s)?(((.+)(\s)?)+)```");
+
+    var matches = regex.allMatches(message.content);
+    for (var m in matches) yield m.group(3);
+  }
 }
