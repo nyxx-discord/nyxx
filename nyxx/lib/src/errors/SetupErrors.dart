8afd6beef6d33609c99f6fa630594481e63fc5ea
Szymon Uglis <szymon.uglis@tuta.io>
2020-04-21 12:14:45 +0200

    Typo [ci skip]

--- nyxx/lib/src/errors/SetupErrors.dart
+++ nyxx/lib/src/errors/SetupErrors.dart
@@ -5,7 +5,7 @@ class NoSetupError implements Exception {
   /// Returns a string representation of this object.
   @override
   String toString() =>
-      "NotSetupError: Bbot isn't set up properly! Use NyxxVm class from Vm package";
+      "NotSetupError: Bot isn't set up properly! Use NyxxVm class from Vm package";
 }
 
 /// Thrown when token is empty or null
