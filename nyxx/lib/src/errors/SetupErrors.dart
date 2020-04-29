103486ce9c980e87703a86b7c7dc5cf72d584cc7
Szymon Uglis <szymon.uglis@tuta.io>
2020-04-29 02:02:41 +0200

    objects package remved to core. Moved several files to more logical locations [ci skip]

--- nyxx/lib/src/errors/SetupErrors.dart
+++ nyxx/lib/src/errors/SetupErrors.dart
@@ -1,13 +1,5 @@
 part of nyxx;
 
-/// Thrown when client isn't set up.
-class NoSetupError implements Exception {
-  /// Returns a string representation of this object.
-  @override
-  String toString() =>
-      "NotSetupError: Bot isn't set up properly! Use NyxxVm class from Vm package";
-}
-
 /// Thrown when token is empty or null
 class NoTokenError implements Exception {
   /// Returns a string representation of this object.
