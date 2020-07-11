f24ed339463f008786287c954e152aade3ca8728
Szymon Uglis <szymon.uglis@tuta.io>
2020-07-11 22:01:16 +0200

    Docs, export util file, fix imports order [ci skip]

--- nyxx.extensions/lib/src/emoji/EmojiDefinition.dart
+++ nyxx.extensions/lib/src/emoji/EmojiDefinition.dart
@@ -1,15 +1,23 @@
 part of nyxx.extensions.emoji;
 
+/// Wrapper class around discords emojis data
 class EmojiDefinition {
+  /// Name of emoji
   late final String primaryName;
+
+  /// List of alternative names of emoji
   late final Iterable<String> names;
 
+  /// Literal emoji
   late final String rawEmoji;
 
+  /// List of utf32 code points
   late final Iterable<int> codePoints;
 
+  /// Name of asset used in discords frontend for this emoji
   late final String assetFileName;
 
+  /// Url of emoji picture
   late final String assetUrl;
 
   EmojiDefinition._new(Map<String, dynamic> raw) {
@@ -21,5 +29,6 @@ class EmojiDefinition {
     this.assetUrl = raw["assetUrl"] as String;
   }
 
+  /// Returns [UnicodeEmoji] object of this
   UnicodeEmoji toEmoji() => UnicodeEmoji(rawEmoji);
 }
