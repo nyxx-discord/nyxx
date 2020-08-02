028784d84848c338c97d935041b2ce704c133f7f
Szymon Uglis <szymon.uglis@tuta.io>
2020-08-02 10:51:57 +0200

    Add missing VoiceState fields [ci skip]

--- nyxx/lib/src/core/voice/VoiceState.dart
+++ nyxx/lib/src/core/voice/VoiceState.dart
@@ -27,6 +27,12 @@ class VoiceState {
   /// Whether this user is muted by the current user
   late final bool suppress;
 
+  /// Whether this user is streaming using "Go Live"
+  late final bool selfStream;
+
+  /// Whether this user's camera is enabled
+  late final bool selfVideo;
+
   VoiceState._new(Map<String, dynamic> raw, Nyxx client, [Guild? guild]) {
     if (raw["channel_id"] != null) {
       this.channel = client.channels[Snowflake(raw["channel_id"])] as CacheVoiceChannel?;
@@ -35,6 +41,10 @@ class VoiceState {
     this.deaf = raw["deaf"] as bool;
     this.selfDeaf = raw["self_deaf"] as bool;
     this.selfMute = raw["self_mute"] as bool;
+
+    this.selfStream = raw["self_stream"] as bool? ?? false;
+    this.selfVideo = raw["self_video"] as bool;
+
     this.suppress = raw["suppress"] as bool;
     this.sessionId = raw["session_id"] as String;
 
