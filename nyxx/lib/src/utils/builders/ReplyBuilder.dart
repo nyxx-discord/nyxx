f7aaebaf90f18dc68af80b11aa2b41ae766ed36a
Szymon Uglis <szymon@l7ssha.xyz>
2020-11-17 17:16:10 +0100

    AllowedMentions update for replies; channel id is not required for reply [ci skip]

--- nyxx/lib/src/utils/builders/ReplyBuilder.dart
+++ nyxx/lib/src/utils/builders/ReplyBuilder.dart
@@ -5,23 +5,19 @@ class ReplyBuilder implements Builder {
   /// Id of message you reply to
   final Snowflake messageId;
 
-  /// Id of channel of message which you reply to
-  final Snowflake channelId;
-
   /// Constructs reply builder for given message in channel
-  ReplyBuilder(this.messageId, this.channelId);
+  ReplyBuilder(this.messageId);
 
   /// Constructs message reply from given message
   factory ReplyBuilder.froMessage(Message message) =>
-      ReplyBuilder(message.id, message.channel.id);
+      ReplyBuilder(message.id);
 
   /// Constructs message reply from cacheable of message and channel
-  factory ReplyBuilder.fromCacheable(Cacheable<Snowflake, Message> messageCacheable, Cacheable<Snowflake, TextChannel> channelCacheable) =>
-    ReplyBuilder(messageCacheable.id, channelCacheable.id);
+  factory ReplyBuilder.fromCacheable(Cacheable<Snowflake, Message> messageCacheable) =>
+    ReplyBuilder(messageCacheable.id);
 
   @override
   Map<String, dynamic> _build() => {
-    "message_id": this.messageId.id,
-    "channel_id": this.channelId.id
+    "message_id": this.messageId.id
   };
 }
