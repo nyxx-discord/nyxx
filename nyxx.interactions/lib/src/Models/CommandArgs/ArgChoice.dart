5828f4a936fd13da10e807f373e016b0b1a1275f
HarryET <harryet@users.noreply.github.com>
2021-02-10 16:29:42 +0000

    Add Dart Docs & Sort final issues.

--- nyxx.interactions/lib/src/Models/CommandArgs/ArgChoice.dart
+++ nyxx.interactions/lib/src/Models/CommandArgs/ArgChoice.dart
@@ -1,31 +1,23 @@
 part of nyxx_interactions;
 
+/// A specified choice for a slash command argument.
 class ArgChoice {
-
   /// This options name.
   late final String name;
 
-  /// This options int value. If there is an int value there can't be a [SlashArgChoice.stringValue]
-  late final int? intValue;
-
-  /// This options string value. If there is an string value there can't be a [SlashArgChoice.intValue]
-  late final String? stringValue;
+  /// This is the options value, must be int or string
+  late final dynamic value;
 
   /// A Choice for the user to input in int & string args. You can only have an int or string option.
   ArgChoice(this.name, dynamic value) {
-    if(value is String) {
-      this.stringValue = value;
+    if (value is String || value is int) {
+      this.value = value;
     }
-    if(value is int) {
-      this.intValue = value;
-    }
-    if(value is! int && value is! String) {
-      throw 'InvalidParamTypeError: Please send a string if its a string arg or an int if its an int arg.';
+
+    if (value is! int && value is! String) {
+      throw "InvalidParamTypeError: Please send a string if its a string arg or an int if its an int arg.";
     }
   }
 
-  Map<String, dynamic> _build() => {
-    "name": this.name,
-    "value": this.stringValue ?? this.intValue
-  };
+  Map<String, dynamic> _build() => {"name": this.name, "value": this.value};
 }
--- nyxx.interactions/lib/src/Models/SlashCommand.dart
+++ nyxx.interactions/lib/src/Models/SlashCommand.dart
@@ -1,12 +1,18 @@
 part of nyxx_interactions;
 
+/// A slash command, can only be instantiated through a method on [Interactions]
 class SlashCommand {
-
+  /// Command name to be shown to the user in the Slash Command UI
   late final String name;
+  /// Command description shown to the user in the Slash Command UI
   late final String description;
+  /// The guild that the slash Command is registered in. This can be null if its a global command.
   late final Cacheable<Snowflake, Guild>? guild;
+  /// The arguments that the command takes
   late final List<CommandArg> args;
+  /// If the command is a global on, false if restricted to a guild.
   late final bool isGlobal;
+  /// If the command has been registered with the discord api
   late bool isRegistered = false;
 
   late final Nyxx _client;
@@ -16,24 +22,19 @@ class SlashCommand {
   }
 
   Future<SlashCommand> _register() async {
-    final options = List<Map<String, dynamic>>.generate(
-        this.args.length, (i) => this.args[i]._build());
+    final options = args.map((e) => e._build());
 
     final response = await this._client.httpEndpoints.sendRawRequest(
       "/applications/${this._client.app.id.toString()}/commands",
       "POST",
-      body: {
-        "name": this.name,
-        "description": this.description,
-        "options": options.isNotEmpty ? options : null
-      },
+      body: {"name": this.name, "description": this.description, "options": options.isNotEmpty ? options : null},
     );
 
     if (response is HttpResponseError) {
       return Future.error(response);
     }
-    this.isRegistered = true;
-    return Future.value(this);
-  }
 
+    this.isRegistered = true;
+    return this;
+  }
 }
