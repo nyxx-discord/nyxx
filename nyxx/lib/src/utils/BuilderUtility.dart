9c08377f72884fa197b7569d394f012f470837db
Szymon Uglis <szymon.uglis@hotmail.com>
2021-03-04 01:11:47 +0100

    Fix style, add simple permission utils tests

--- nyxx/lib/src/utils/BuilderUtility.dart
+++ nyxx/lib/src/utils/BuilderUtility.dart
@@ -2,13 +2,11 @@ part of nyxx;
 
 /// A utility to run ._build() on internal functions
 class BuilderUtility {
-
   /// Build the data from an [EmbedBuilder]
   static Map<String, dynamic> buildRawEmbed(EmbedBuilder embed) =>
       embed._build();
 
   /// Build the data from [AllowedMentions]
-  static Map<String, dynamic> buildRawAllowedMentions(
-          AllowedMentions mentions) =>
+  static Map<String, dynamic> buildRawAllowedMentions(AllowedMentions mentions) =>
       mentions._build();
 }
