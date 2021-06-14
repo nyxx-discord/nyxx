c2145bc053827a5f2972c2c9fac145dcd61d7a65
Szymon Uglis <szymon@l7ssha.xyz>
2021-06-14 18:49:57 +0200

    Add missing data from thread members update event

--- nyxx/lib/src/events/ThreadMembersUpdateEvent.dart
+++ nyxx/lib/src/events/ThreadMembersUpdateEvent.dart
@@ -9,19 +9,30 @@ class ThreadMembersUpdateEvent {
   late final Cacheable<Snowflake, Guild> guild;
 
   /// The members that were added. Note that they are not cached
-  late final List<Cacheable<Snowflake, Member>> addedMembers;
+  late final Iterable<Cacheable<Snowflake, Member>> addedMembers;
+
+  /// The approximate number of members in the thread, capped at 50
+  late final int approxMemberCount;
+
+  /// Users who were removed from the thread
+  late final Iterable<Cacheable<Snowflake, User>> removedUsers;
 
   ThreadMembersUpdateEvent._new(RawApiMap raw, Nyxx client) {
     final data = raw["d"] as RawApiMap;
 
-    this.thread = new CacheableTextChannel._new(client, Snowflake(data["id"]));
-    this.guild = new _GuildCacheable(client, Snowflake(data["guild_id"]));
+    this.thread = CacheableTextChannel._new(client, Snowflake(data["id"]));
+    this.guild = _GuildCacheable(client, Snowflake(data["guild_id"]));
 
-    addedMembers = [];
-    if(data["added_members"] != null) {
-      for(final memberData in data["added_members"] as List<dynamic>) {
-        addedMembers.add(new _MemberCacheable(client, Snowflake(memberData["user_id"]), this.guild));
-      }
-    }
+    this.addedMembers = [
+      if(data["added_members"] != null)
+        for(final memberData in data["added_members"] as List<dynamic>)
+          _MemberCacheable(client, Snowflake(memberData["user_id"]), this.guild)
+    ];
+
+    this.removedUsers = [
+      if(data["removed_member_ids"] != null)
+        for(final removedUserId in data["removed_member_ids"])
+          _UserCacheable(client, Snowflake(removedUserId))
+    ];
   }
 }
