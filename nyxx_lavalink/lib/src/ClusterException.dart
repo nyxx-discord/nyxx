3e91d60ccee4c89aaa3329c6262be19b66f5bb3e
AlvaroMS_25 <alvaro2ms5@gmail.com>
2021-06-19 20:29:45 +0200

    final

--- nyxx_lavalink/lib/src/ClusterException.dart
+++ nyxx_lavalink/lib/src/ClusterException.dart
@@ -3,7 +3,7 @@ part of nyxx_lavalink;
 /// An exception related to cluster functions
 class ClusterException implements Exception {
   /// The actual error description
-  String error;
+  final String error;
   
   ClusterException._new(this.error);
 
