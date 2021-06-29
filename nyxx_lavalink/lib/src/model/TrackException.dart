d7ed703d884cf2736499419e7302b88c65b5aeb2
AlvaroMS_25 <alvaro2ms5@gmail.com>
2021-06-29 16:41:19 +0200

    renamed from error to exception

--- nyxx_lavalink/lib/src/model/TrackException.dart
+++ nyxx_lavalink/lib/src/model/TrackException.dart
@@ -5,13 +5,13 @@ class TrackExceptionEvent extends BaseEvent {
   /// Base64 encoded track
   late final String track;
   /// The occurred error
-  late final LavalinkException error;
+  late final LavalinkException exception;
   /// Guild id where the track got an exception
   late final Snowflake guildId;
 
   TrackExceptionEvent._fromJson(Nyxx client, Node node, Map<String, dynamic> json) : super(client, node) {
     this.track = json["track"] as String;
-    this.error = LavalinkException._fromJson(json);
+    this.exception = LavalinkException._fromJson(json);
     this.guildId = Snowflake(json["guildId"]);
   }
 }
