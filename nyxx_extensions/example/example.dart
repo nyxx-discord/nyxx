41537bab98fda3d9f6e44a1ba8e87800aa30659a
Szymon Uglis <szymon@l7ssha.xyz>
2021-07-24 10:57:22 +0200

    Add pagination example

--- nyxx_extensions/example/example.dart
+++ nyxx_extensions/example/example.dart
@@ -1,10 +1,11 @@
 import "package:nyxx/nyxx.dart";
 import "package:nyxx_extensions/emoji.dart" as emoji_extension;
+import "package:nyxx_extensions/pagination.dart" as pagination_extension;
 import "package:nyxx_extensions/src/message_resolver/message_resolver.dart" as message_resolver_extension;
 
 // nyxx.extensions contains several different extensions
 // that could simplify making and implementing bots.
-void main() {
+void main() async {
   // Emoji extension would allow to download and fetch discord emoji definitions
   // from resource shared by Emzi.
   // Emoji utils can cache results to do not download json document each time
@@ -26,5 +27,25 @@ void main() {
   // MessageResolver constructor.
   final resolvedMessage = messageResolver.resolve("This is raw content. <!@123>");
 
+  // Next extension if pagination handler which allow to create messages that can
+  // have multiple pages of content and can be switched by "buttons".
+  // To use that extension provide channel where paginated message will be sent
+  // and implementation of IPaginationHandler.
+  // nyxx_extensions provides few implementation of that interface which allows to
+  // display basic text pages.
+  final paginationChannel = await bot.fetchChannel<TextChannel>(Snowflake(123));
+  final pages = [
+    "this is one page",
+    "this is second page",
+    "this is third page",
+  ];
 
+  // Create instance of pagination and provide necessary arguments. Then use
+  // .paginate method to send message and start listening to user actions.
+  // Paginate returns message object of resulting message
+  final paginator = pagination_extension.Pagination(
+      paginationChannel,
+      pagination_extension.BasicPaginationHandler(pages)
+  );
+  final resultingMessage = await paginator.paginate(bot);
 }
