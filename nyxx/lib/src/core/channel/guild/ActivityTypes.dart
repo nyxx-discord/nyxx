12e125b95bc813e88738eab6bb79020b44a7f8ca
HarryET <29015545+HarryET@users.noreply.github.com>
2021-10-01 18:06:52 +0100

    Remove extra spaces

--- nyxx/lib/src/core/channel/guild/ActivityTypes.dart
+++ nyxx/lib/src/core/channel/guild/ActivityTypes.dart
@@ -3,20 +3,13 @@ part of nyxx;
 /// Activity Types
 class VoiceActivityType extends IEnum<String> {
   static const VoiceActivityType youtubeTogether = VoiceActivityType._create("755600276941176913");
-
-	static const VoiceActivityType poker = VoiceActivityType._create("755827207812677713");
-
-	static const VoiceActivityType betrayal = VoiceActivityType._create("773336526917861400");
-
-	static const VoiceActivityType fishing = VoiceActivityType._create("814288819477020702");
-
-	static const VoiceActivityType chess = VoiceActivityType._create("832012774040141894");
-
-	static const VoiceActivityType letterTile = VoiceActivityType._create("879863686565621790");
-
-	static const VoiceActivityType wordSnack = VoiceActivityType._create("879863976006127627");
-
-	static const VoiceActivityType doodleCrew = VoiceActivityType._create("878067389634314250");
+  static const VoiceActivityType poker = VoiceActivityType._create("755827207812677713");
+  static const VoiceActivityType betrayal = VoiceActivityType._create("773336526917861400");
+  static const VoiceActivityType fishing = VoiceActivityType._create("814288819477020702");
+  static const VoiceActivityType chess = VoiceActivityType._create("832012774040141894");
+  static const VoiceActivityType letterTile = VoiceActivityType._create("879863686565621790");
+  static const VoiceActivityType wordSnack = VoiceActivityType._create("879863976006127627");
+  static const VoiceActivityType doodleCrew = VoiceActivityType._create("878067389634314250");
 
   /// Creates instance of [GuildFeature] from [value].
   VoiceActivityType.from(String? value) : super(value ?? "");
