65dd77ab9fe98b5845a050aeef298a33184a5478
HarryET <29015545+HarryET@users.noreply.github.com>
2021-10-01 09:53:28 +0100

    Add Student Hub types
    Currently not much use as Bots cannot join student hubs.

--- nyxx/lib/src/core/guild/GuildFeature.dart
+++ nyxx/lib/src/core/guild/GuildFeature.dart
@@ -56,6 +56,9 @@ class GuildFeature extends IEnum<String> {
   /// Guild has access to create private threads
   static const GuildFeature privateThreadsEnabled = GuildFeature._create("PRIVATE_THREADS");
 
+	/// Guild is a Student Hub
+  static const GuildFeature studentHub = GuildFeature._create("HUB");
+
   /// Creates instance of [GuildFeature] from [value].
   GuildFeature.from(String? value) : super(value ?? "");
   const GuildFeature._create(String? value) : super(value ?? "");
