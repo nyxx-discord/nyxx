96f2d1d37c1c2eb844e12c3f8f2963feb2578833
Harry Bairstow <h.bairstow22@gmail.com>
2021-10-23 10:00:39 +0100

    Initial Unit Rewrite
    * Remove old Unit tests
    * Add start of new unit tests

---
+++ nyxx/test/unit/Message.mocks.dart
@@ -0,0 +1,12 @@
+import 'package:mockito/mockito.dart';
+import 'package:nyxx/nyxx.dart';
+
+class MockMessage extends SnowflakeEntity with Fake implements IMessage {
+	/// The message's content.
+  @override
+  late String content;
+
+  MockMessage(RawApiMap rawData, Snowflake id) : super(id) {
+		this.content = rawData["content"] as String;
+	}
+}
