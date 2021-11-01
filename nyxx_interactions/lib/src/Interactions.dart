8262ab37304a8766aad54c9836be2bbb8866bf11
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-11-02 00:13:19 +0100

    Allow handlers on different nesting layers, closes #233

--- nyxx_interactions/lib/src/Interactions.dart
+++ nyxx_interactions/lib/src/Interactions.dart
@@ -241,6 +241,8 @@ class Interactions {
   void _assignCommandToHandler(SlashCommandBuilder builder, SlashCommand command) {
     final commandHashPrefix = "${command.id}|${command.name}";
 
+    var allowRootHandler = true;
+
     final subCommands = builder.options.where((element) => element.type == CommandOptionType.subCommand);
     if (subCommands.isNotEmpty) {
       for (final subCommand in subCommands) {
@@ -251,7 +253,7 @@ class Interactions {
         this._commandHandlers["$commandHashPrefix|${subCommand.name}"] = subCommand._handler!;
       }
 
-      return;
+      allowRootHandler = false;
     }
 
     final subCommandGroups = builder.options.where((element) => element.type == CommandOptionType.subCommandGroup);
@@ -269,6 +271,10 @@ class Interactions {
         }
       }
 
+      allowRootHandler = false;
+    }
+
+    if (!allowRootHandler) {
       return;
     }
 
