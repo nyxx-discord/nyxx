b3e590685d973221810eb4e14b8c7bf1ee59f0a7
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-03-02 09:12:25 +0100

    Fix new line at the end

--- example/emojis.dart
+++ example/emojis.dart
@@ -24,9 +24,11 @@ void main(List<String> args) {
   // This event is called when a reaction has been added to a message
   bot.eventsWs.onMessageReactionAdded.listen((event) {
     if (event.emoji is UnicodeEmoji) {
-      event.message?.channel.sendMessage(MessageBuilder.content(
-        'Woah! This is a unicode emoji: ${event.emoji}',
-      ));
+      event.message?.channel.sendMessage(
+        MessageBuilder.content(
+          'Woah! This is a unicode emoji: ${event.emoji}',
+        ),
+      );
     } else if (event.emoji is IGuildEmojiPartial) {
       final emoji = (event.emoji as IGuildEmojiPartial).resolve();
       if (emoji?.name == 'some_emoji') {
