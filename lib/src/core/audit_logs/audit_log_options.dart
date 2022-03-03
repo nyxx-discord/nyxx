13aa7b12893c088453bcd6165c9861f8ba36b704
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-03-03 10:46:07 +0100

    Remove action types doc

--- lib/src/core/audit_logs/audit_log_options.dart
+++ lib/src/core/audit_logs/audit_log_options.dart
@@ -3,55 +3,30 @@ import 'package:nyxx/nyxx.dart';
 /// Additionnal info for certain [action types](https://discord.com/developers/docs/resources/audit-log#audit-log-entry-object-audit-log-events)
 abstract class IAuditLogOptions {
   /// The channel in which the entites were targeted.
-  /// 
-  /// Shows only in: 
-  /// - [AuditLogEntryType.memberMove]; 
-  /// - [AuditLogEntryType.messagePin];
-  /// - [AuditLogEntryType.messageUnpin];
-  /// - [AuditLogEntryType.messageDelete];
-  /// - [AuditLogEntryType.stageInstanceCreate];
-  /// - [AuditLogEntryType.stageInstanceUpdate] and [AuditLogEntryType.stageInstanceDelete] action types
   Snowflake? get channelId;
 
   /// The number of entities targeted.
-  /// Shows only in: 
-  /// - [AuditLogEntryType.messageDelete];
-  /// - [AuditLogEntryType.messageBulkDelete];
-  /// - [AuditLogEntryType.memberDisconnect] and [AuditLogEntryType.memberMove] action types 
   int? get count;
 
   /// The number of days after which inactive users will be kicked.
-  /// Shows only in: [AuditLogEntryType.memberPrune] action types
   String? get deleteMemberDelay;
 
   /// Id of the overwritten entity.
-  /// Shows only in: 
-  /// - [AuditLogEntryType.channelOverwriteCreate];
-  /// - [AuditLogEntryType.channelOverwriteUpdate] and [AuditLogEntryType.channelOverwriteDelete] action types
   Snowflake? get id;
 
   /// The number of the members removed by the prune.
-  /// Shows only in: [AuditLogEntryType.memberPrune] action types
   int? get pruneCount;
 
   /// The id of the message that was targeted.
-  /// Shows only in: [AuditLogEntryType.messagePin] and [AuditLogEntryType.messageUnpin] action types
   Snowflake? get messageId;
 
   /// The name of the role that was targeted. (Not present if [overwrittenType] is `member`).
-  /// Shows only in: 
-  /// - [AuditLogEntryType.channelOverwriteCreate];
-  /// - [AuditLogEntryType.channelOverwriteUpdate] and [AuditLogEntryType.channelOverwriteDelete] action types
   String? get roleName;
 
   /// Type of overwritten entity.
   /// One of: 
   ///   - `role`
   ///   - `member`
-  ///
-  /// Shows only in: 
-  /// - [AuditLogEntryType.channelOverwriteCreate];
-  /// - [AuditLogEntryType.channelOverwriteUpdate] and [AuditLogEntryType.channelOverwriteDelete] action types
   String? get overwrittenType;
 
   /// The constructor for the [IAuditLogOptions]
