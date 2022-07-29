f5a0eee06dd492294024d0c70e0507db72673e7b
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-07-29 16:34:08 +0200

    Port events for audit logs

--- lib/src/core/audit_logs/audit_log.dart
+++ lib/src/core/audit_logs/audit_log.dart
@@ -1,5 +1,6 @@
 import 'package:nyxx/src/core/audit_logs/audit_log_entry.dart';
 import 'package:nyxx/src/core/guild/auto_moderation.dart';
+import 'package:nyxx/src/core/guild/scheduled_event.dart';
 import 'package:nyxx/src/core/guild/webhook.dart';
 import 'package:nyxx/src/core/snowflake.dart';
 import 'package:nyxx/src/core/user/user.dart';
@@ -7,17 +8,20 @@ import 'package:nyxx/src/nyxx.dart';
 import 'package:nyxx/src/typedefs.dart';
 
 abstract class IAuditLog {
-  /// Map of webhooks found in the audit log
+  /// Map of webhooks found in the audit log.
   late final Map<Snowflake, IWebhook> webhooks;
 
-  /// Map of users found in the audit log
+  /// Map of users found in the audit log.
   late final Map<Snowflake, IUser> users;
 
-  /// Map of audit log entries
+  /// Map of audit log entries.
   late final Map<Snowflake, IAuditLogEntry> entries;
-  
-  /// Map of auto moderation rules referenced in the audit log
+
+  /// Map of auto moderation rules referenced in the audit log.
   late final Map<Snowflake, IAutoModerationRule> autoModerationRules;
+
+  /// Map of guild scheduled events referenced in the audit log.
+  late final Map<Snowflake, IGuildEvent> events;
 
   /// Filters audit log by [users]
   Iterable<IAuditLogEntry> filter(bool Function(IAuditLogEntry) test);
@@ -43,6 +47,10 @@ class AuditLog implements IAuditLog {
   @override
   late final Map<Snowflake, IAutoModerationRule> autoModerationRules;
 
+  /// Map of guild scheduled events referenced in the audit log
+  @override
+  late final Map<Snowflake, IGuildEvent> events;
+
   /// Creates an instance of [AuditLog]
   AuditLog(RawApiMap raw, INyxx client) {
     webhooks = {};
@@ -63,6 +71,10 @@ class AuditLog implements IAuditLog {
 
     raw['auto_moderation_rules'].forEach((o) {
       autoModerationRules[Snowflake(o['id'] as String)] = AutoModerationRule(o as RawApiMap, client);
+    });
+
+    raw['guild_scheduled_events'].forEach((o) {
+      events[Snowflake(o['id'] as String)] = GuildEvent(o as RawApiMap, client);
     });
   }
 
