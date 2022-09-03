0a7147e10eb1711b73bbcbffd6bab3ecd1ae8577
Szymon Uglis <szymon@l7ssha.xyz>
2022-09-03 21:41:55 +0200

    Release 4.1.0-dev.3

--- lib/src/internal/constants.dart
+++ lib/src/internal/constants.dart
@@ -33,7 +33,7 @@ class Constants {
   static const int apiVersion = 10;
 
   /// Version of Nyxx
-  static const String version = "4.1.0-dev.2";
+  static const String version = "4.1.0-dev.3";
 
   /// Url to Nyxx repo
   static const String repoUrl = "https://github.com/nyxx-discord/nyxx";
