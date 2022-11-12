6cb667afb117ce606bcfaed685d79b77fc4c1f4e
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-11-12 23:49:04 +0100

    fix: add message content to client (#389)

--- example/create_add_role.dart
+++ example/create_add_role.dart
@@ -3,7 +3,7 @@ import "package:nyxx/nyxx.dart";
 // Main function
 void main() async {
   // Create new bot instance. Replace string with your token
-  final bot = NyxxFactory.createNyxxWebsocket("<TOKEN>", GatewayIntents.allUnprivileged)
+  final bot = NyxxFactory.createNyxxWebsocket("<TOKEN>", GatewayIntents.allUnprivileged | GatewayIntents.messageContent) // Here we use the privilegied intent message content to receive incoming messages.
     ..registerPlugin(Logging()) // Default logging plugin
     ..registerPlugin(CliIntegration()) // Cli integration for nyxx allows stopping application via SIGTERM and SIGKILl
     ..registerPlugin(IgnoreExceptions()) // Plugin that handles uncaught exceptions that may occur
@@ -20,7 +20,7 @@ void main() async {
     if (e.message.content == "!role") {
 
       // Make sure that message was sent in guild not im dm, because we cant add roles in dms
-      if(e.message.guild != null) {
+      if (e.message.guild != null) {
         return;
       }
 
--- example/emojis.dart
+++ example/emojis.dart
@@ -1,38 +1,40 @@
 import 'package:nyxx/nyxx.dart';
 
 void main(List<String> args) {
-    // Create new bot instance. Replace string with your token
-  final bot = NyxxFactory.createNyxxWebsocket("<TOKEN>", GatewayIntents.allUnprivileged)
+  // Create new bot instance. Replace string with your token
+  final bot = NyxxFactory.createNyxxWebsocket("<TOKEN>", GatewayIntents.allUnprivileged | GatewayIntents.messageContent) // Here we use the privilegied intent message content to receive incoming messages.
     ..registerPlugin(Logging()) // Default logging plugin
     ..registerPlugin(CliIntegration()) // Cli integration for nyxx allows stopping application via SIGTERM and SIGKILl
     ..registerPlugin(IgnoreExceptions()) // Plugin that handles uncaught exceptions that may occur
     ..connect();
+
   bot.eventsWs.onReady.listen((_) {
     print('Ready!');
   });
+
   // This event is called when a message is received
   bot.eventsWs.onMessageReceived.listen((event) async {
-    if(event.message.content == '!emoji') {
+    if (event.message.content == '!emoji') {
       final emoji = event.message.guild?.getFromCache()?.emojis.values.firstWhere((emo) => emo.name == 'nyxx');
       final msg = await event.message.channel.sendMessage(MessageBuilder.content('Look at this emoji: $emoji'));
-      msg.createReaction(emoji!);
+      await msg.createReaction(emoji!);
       // For unicode emoji use `UnicodeEmoji` class
-      msg.createReaction(UnicodeEmoji('🤔'));
+      await msg.createReaction(UnicodeEmoji('🤔'));
     }
   });
 
   // This event is called when a reaction has been added to a message
-  bot.eventsWs.onMessageReactionAdded.listen((event) {
+  bot.eventsWs.onMessageReactionAdded.listen((event) async {
     if (event.emoji is UnicodeEmoji) {
-      event.message?.channel.sendMessage(
+      await event.message?.channel.sendMessage(
         MessageBuilder.content(
           'Woah! This is a unicode emoji: ${event.emoji}',
         ),
       );
     } else if (event.emoji is IGuildEmojiPartial) {
-      if(event.emoji is IResolvableGuildEmojiPartial) {
+      if (event.emoji is IResolvableGuildEmojiPartial) {
         final emoji = (event.emoji as IResolvableGuildEmojiPartial).resolve();
-        event.message?.channel.sendMessage(
+        await event.message?.channel.sendMessage(
           MessageBuilder.content(
             'Woah! This is a custom emoji: ${emoji.name}',
           ),
