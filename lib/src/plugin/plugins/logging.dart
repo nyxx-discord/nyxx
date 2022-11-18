c9ea243f1164e6f23ca685d38612cd5708d702d1
Abitofevrything <mylo.fawcett@gmail.com>
2022-11-18 15:07:12 +0100

    Improve logging plugin

--- lib/src/plugin/plugins/logging.dart
+++ lib/src/plugin/plugins/logging.dart
@@ -1,12 +1,74 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:logging/logging.dart';
 import 'package:nyxx/src/nyxx.dart';
 import 'package:nyxx/src/plugin/plugin.dart';
 
 class Logging extends BasePlugin {
+  final Level stderrLevel;
+  final Level stackTraceLevel;
+  final Level logLevel;
+
+  final int? truncateLogsAt;
+
+  Logging({
+    this.stderrLevel = Level.WARNING,
+    this.stackTraceLevel = Level.SEVERE,
+    this.logLevel = Level.INFO,
+    this.truncateLogsAt = 1000,
+  });
+
+  Level? _oldStacktraceLevel;
+  Level? _oldLogLevel;
+  StreamSubscription<void>? _subscription;
+
   @override
   void onRegister(INyxx nyxx, Logger logger) {
-    Logger.root.onRecord.listen((LogRecord rec) {
-      print("[${rec.time}] [${rec.level.name}] [${rec.loggerName}] ${rec.message}");
+    _oldStacktraceLevel = recordStackTraceAtLevel;
+    _oldLogLevel = Logger.root.level;
+
+    recordStackTraceAtLevel = stackTraceLevel;
+    Logger.root.level = logLevel;
+
+    _subscription = Logger.root.onRecord.listen((LogRecord rec) {
+      final message = StringBuffer();
+
+      var customMessage = rec.message;
+      if (truncateLogsAt != null && customMessage.length > truncateLogsAt!) {
+        customMessage = '${customMessage.substring(0, truncateLogsAt! - 3)}...';
+      }
+
+      message.writeln('[${rec.time}] [${rec.level.name}] [${rec.loggerName}] $customMessage');
+
+      final error = rec.error;
+      if (error != null) {
+        // Indent error
+        final errorMessage = 'Error: $error'.replaceAll('\n', '\n  ');
+        message.writeln(errorMessage);
+
+        if (errorMessage.contains('\n')) {
+          // Add newline for extra readability if error message contains newlines
+          message.writeln();
+        }
+      }
+
+      final stackTrace = (error is Error ? error.stackTrace : null) ?? rec.stackTrace;
+      if (stackTrace != null) {
+        // Indent stack trace
+        message.writeln('Stack trace:\n$stackTrace'.replaceAll('\n', '\n  '));
+        message.writeln();
+      }
+
+      final outSink = rec.level > stderrLevel ? stderr : stdout;
+      outSink.write(message.toString());
     });
+  }
+
+  @override
+  void onBotStop(INyxx nyxx, Logger logger) {
+    recordStackTraceAtLevel = _oldStacktraceLevel!;
+    Logger.root.level = _oldLogLevel!;
+    _subscription!.cancel();
   }
 }
