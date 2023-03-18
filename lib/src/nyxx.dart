56bd77d7a6cddb4b1efdbc99409b20df221bcc69
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2023-03-18 16:20:23 +0100

    Correctly dispose all resources on bot stop (#451)
    * Don't heartbeat after bot is disposed
    
    * Dispose HTTP client on bot stop

--- lib/src/nyxx.dart
+++ lib/src/nyxx.dart
@@ -227,8 +227,9 @@ class NyxxRest extends INyxxRest {
     }
 
     await eventsRest.dispose();
+    await httpHandler.dispose();
 
-    onReadyController.close();
+    await onReadyController.close();
 
     await guilds.dispose();
     await users.dispose();
