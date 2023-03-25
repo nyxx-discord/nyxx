195168c117cb1bed82e9555f0333458218b9b3a5
Abitofevrything <mylo.fawcett@gmail.com>
2023-03-25 19:53:58 +0100

    Export channel models & manager

--- lib/nyxx.dart
+++ lib/nyxx.dart
@@ -15,6 +15,7 @@ export 'src/http/response.dart' show FieldError, HttpErrorData, HttpResponse, Ht
 export 'src/http/route.dart' show HttpRoute, HttpRouteParam, HttpRoutePart;
 export 'src/http/managers/manager.dart' show Manager, ReadOnlyManager;
 export 'src/http/managers/user_manager.dart' show UserManager;
+export 'src/http/managers/channel_manager.dart' show ChannelManager;
 
 export 'src/models/discord_color.dart' show DiscordColor;
 export 'src/models/locale.dart' show Locale;
@@ -23,6 +24,24 @@ export 'src/models/snowflake_entity/snowflake_entity.dart' show SnowflakeEntity;
 export 'src/models/user/application_role_connection.dart' show ApplicationRoleConnection;
 export 'src/models/user/connection.dart' show Connection, ConnectionType, ConnectionVisibility;
 export 'src/models/user/user.dart' show PartialUser, User, UserFlags, NitroType;
+export 'src/models/channel/channel.dart' show Channel, ChannelFlags, PartialChannel, ChannelType;
+export 'src/models/channel/guild_channel.dart' show GuildChannel, PartialGuildChannel;
+export 'src/models/channel/has_threads_channel.dart' show HasThreadsChannel, PartialHasThreadsChannel;
+export 'src/models/channel/text_channel.dart' show PartialTextChannel, TextChannel;
+export 'src/models/channel/thread.dart' show PartialThread, PartialThreadMember, Thread, ThreadMember;
+export 'src/models/channel/voice_channel.dart' show PartialVoiceChannel, VoiceChannel, VideoQualityMode;
+export 'src/models/channel/types/announcement_thread.dart' show AnnouncementThread, PartialAnnouncementThread;
+export 'src/models/channel/types/directory.dart' show DirectoryChannel, PartialDirectoryChannel;
+export 'src/models/channel/types/dm.dart' show DmChannel, PartialDmChannel;
+export 'src/models/channel/types/forum.dart' show DefaultReaction, ForumChannel, ForumTag, PartialForumChannel, ForumLayout, ForumSort;
+export 'src/models/channel/types/group_dm.dart' show GroupDmChannel, PartialGroupDmChannel;
+export 'src/models/channel/types/guild_announcement.dart' show GuildAnnouncementChannel, PartialGuildAnnouncementChannel;
+export 'src/models/channel/types/guild_category.dart' show GuildCategory, PartialGuildCategory;
+export 'src/models/channel/types/guild_stage.dart' show GuildStageChannel, PartialGuildStageChannel;
+export 'src/models/channel/types/guild_text.dart' show GuildTextChannel, PartialGuildTextChannel;
+export 'src/models/channel/types/guild_voice.dart' show GuildVoiceChannel, PartialGuildVoiceChannel;
+export 'src/models/channel/types/private_thread.dart' show PartialPrivateThread, PrivateThread;
+export 'src/models/channel/types/public_thread.dart' show PartialPublicThread, PublicThread;
 
 // Types also used in the nyxx API from other packages
 export 'package:http/http.dart'
