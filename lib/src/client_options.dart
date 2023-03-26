d5a112f279fb8fdc1a9a5afdf01bb16459f4d9ee
Abitofevrything <mylo.fawcett@gmail.com>
2023-03-26 22:24:36 +0200

    Add channels manager to client

--- lib/src/client_options.dart
+++ lib/src/client_options.dart
@@ -1,4 +1,6 @@
 import 'package:nyxx/src/cache/cache.dart';
+import 'package:nyxx/src/models/channel/channel.dart';
+import 'package:nyxx/src/models/message/message.dart';
 import 'package:nyxx/src/models/user/user.dart';
 
 /// Options for controlling the behavior of a [Nyxx] client.
@@ -9,6 +11,14 @@ class RestClientOptions implements ClientOptions {
   /// The [CacheConfig] to use for the cache of the [NyxxRest.users] manager.
   final CacheConfig<User> userCacheConfig;
 
+  final CacheConfig<Channel> channelCacheConfig;
+
+  final CacheConfig<Message> messageCacheConfig;
+
   /// Create a new [RestClientOptions].
-  RestClientOptions({this.userCacheConfig = const CacheConfig()});
+  RestClientOptions({
+    this.userCacheConfig = const CacheConfig(),
+    this.channelCacheConfig = const CacheConfig(),
+    this.messageCacheConfig = const CacheConfig(),
+  });
 }
--- lib/src/manager_mixin.dart
+++ lib/src/manager_mixin.dart
@@ -1,5 +1,6 @@
 import 'package:nyxx/src/client.dart';
 import 'package:nyxx/src/client_options.dart';
+import 'package:nyxx/src/http/managers/channel_manager.dart';
 import 'package:nyxx/src/http/managers/user_manager.dart';
 
 /// An internal mixin to add managers to a [Nyxx] instance.
@@ -9,4 +10,6 @@ mixin ManagerMixin implements Nyxx {
 
   /// A [UserManager] that manages users for this client.
   late final UserManager users = UserManager(options.userCacheConfig, this as NyxxRest);
+
+  late final ChannelManager channels = ChannelManager(options.channelCacheConfig, this as NyxxRest);
 }
