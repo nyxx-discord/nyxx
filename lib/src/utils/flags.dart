34a35fa30922e0580dae085ce32b56d425d38677
Abitofevrything <mylo.fawcett@gmail.com>
2023-03-26 15:04:15 +0200

    Allow flags to be combined

--- lib/src/utils/flags.dart
+++ lib/src/utils/flags.dart
@@ -1,6 +1,17 @@
+abstract class FlagCombinable<T extends Flags<T>> {
+  int get value;
+
+  const FlagCombinable();
+
+  Flags<T> operator |(FlagCombinable<T> other) => Flags(value | other.value);
+
+  Flags<T> operator &(FlagCombinable<T> other) => Flags(value & other.value);
+}
+
 /// A set of flags that can be either enabled or disabled.
-class Flags<T extends Flags<T>> {
+class Flags<T extends Flags<T>> extends FlagCombinable<T> {
   /// The integer value encoding the flags as a bitfield.
+  @override
   final int value;
 
   /// Create a new [Flags].
@@ -17,8 +28,9 @@ class Flags<T extends Flags<T>> {
 }
 
 /// A flag within a set of [Flags].
-class Flag<T extends Flags<T>> {
+class Flag<T extends Flags<T>> extends FlagCombinable<T> {
   /// The value of this flag.
+  @override
   final int value;
 
   /// Create a new [Flag].
@@ -26,4 +38,10 @@ class Flag<T extends Flags<T>> {
 
   /// Create a new [Flag] from an offset into the bitfield.
   const Flag.fromOffset(int offset) : value = 1 << offset;
+
+  @override
+  bool operator ==(Object other) => identical(this, other) || (other is Flag<T> && other.value == value);
+
+  @override
+  int get hashCode => value.hashCode;
 }
