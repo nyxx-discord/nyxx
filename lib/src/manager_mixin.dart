7afc8056012a6c28499430f6998d4f933965be0d
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2023-05-04 20:59:58 +0200

    Rewrite http application api (#477)
    * Create models for channels
    
    * Add channel manager
    
    * Export channel models & manager
    
    * Add tests for ChannelManager
    
    * Fix bugs found by tests
    
    * Add maybeParseMany parsing helper
    
    * Add flags to threads
    
    * Add channel update method
    
    * Allow flags to be combined
    
    * Allow single flags to be used as a set of flags
    
    * Add channel tests
    
    * Fix issues found by tests
    
    * Add channel delete method
    
    * Add message object classes
    
    * Add channels manager to client
    
    * Implement message parsing
    
    * Implement channel endpoints
    
    * Simplify channel model structure
    
    * Move sentinels to their own files
    
    * Use specific builder types
    
    * Add message manager to channels
    
    * Add helper shortcuts
    
    * Correct test folder name
    
    * Give each fetch test a unique name
    
    * Add channel endpoint tests
    
    * Add additional ChannelManager parsing tests
    
    * Add message manager test
    
    * Add missing message endpoint tests
    
    * Fix issues found by tests
    
    * Remove embed.type field
    
    * Add builder tests for channels
    
    * Add extra tests & fix bugs found by tests
    
    * Update test
    
    * Remove async test
    
    * Only load channel id if environment variable is provided
    
    * Move channel methods to concrete subclasses
    
    * Normalize cache accesses between resources of the same type
    
    * Prefer getters over late final
    
    * Remove FlagsCombinable as it is no longer needed
    
    * Start adding documentation to channel classes
    
    * Docuument channel and message managers
    
    * Implement the HTTP Application API
    
    * Add application manager to client

--- lib/src/manager_mixin.dart
+++ lib/src/manager_mixin.dart
@@ -1,5 +1,6 @@
 import 'package:nyxx/src/client.dart';
 import 'package:nyxx/src/client_options.dart';
+import 'package:nyxx/src/http/managers/application_manager.dart';
 import 'package:nyxx/src/http/managers/channel_manager.dart';
 import 'package:nyxx/src/http/managers/user_manager.dart';
 import 'package:nyxx/src/http/managers/webhook_manager.dart';
@@ -14,6 +15,9 @@ mixin ManagerMixin implements Nyxx {
 
   /// A [ChannelManager] that manages channels for this client.
   ChannelManager get channels => ChannelManager(options.channelCacheConfig, this as NyxxRest);
+
+  /// An [ApplicationManager] that manages applications for this client.
+  ApplicationManager get applications => ApplicationManager(this as NyxxRest);
 
   /// A [WebhookManager] that manages webhooks for this client.
   WebhookManager get webhooks => WebhookManager(options.webhookCacheConfig, this as NyxxRest);
---
+++ lib/src/models/team.dart
@@ -0,0 +1,78 @@
+import 'package:nyxx/src/models/snowflake.dart';
+import 'package:nyxx/src/models/user/user.dart';
+import 'package:nyxx/src/utils/to_string_helper/to_string_helper.dart';
+
+/// {@template team}
+/// A group of developers.
+///
+/// External references:
+/// * Discord API Reference: https://discord.com/developers/docs/topics/teams#data-models-team-object
+/// {@endtemplate}
+class Team with ToStringHelper {
+  /// The hash of this team's icon.
+  final String? iconHash;
+
+  /// This team's ID.
+  final Snowflake id;
+
+  /// The members of this team.
+  final List<TeamMember> members;
+
+  /// The name of this team.
+  final String name;
+
+  /// The ID of the owner of this team.
+  final Snowflake ownerId;
+
+  /// {@macro team}
+  Team({
+    required this.iconHash,
+    required this.id,
+    required this.members,
+    required this.name,
+    required this.ownerId,
+  });
+}
+
+/// {@template team_member}
+/// A member of a [Team].
+/// {@endtemplate}
+class TeamMember with ToStringHelper {
+  /// This team member's membership status.
+  final TeamMembershipState membershipState;
+
+  /// The ID of the team this member belongs to.
+  final Snowflake teamId;
+
+  /// The user associated with this team member.
+  final PartialUser user;
+
+  /// {@macro team_member}
+  TeamMember({
+    required this.membershipState,
+    required this.teamId,
+    required this.user,
+  });
+}
+
+/// The status of a member in a [Team].
+enum TeamMembershipState {
+  invited._(1),
+  accepted._(2);
+
+  /// The value of this [TeamMembershipState].
+  final int value;
+
+  const TeamMembershipState._(this.value);
+
+  /// Parse a [TeamMembershipState] from a [value].
+  ///
+  /// The [value] must be a valid team membership state.
+  factory TeamMembershipState.parse(int value) => TeamMembershipState.values.firstWhere(
+        (state) => state.value == value,
+        orElse: () => throw FormatException('Unknown team membership state', value),
+      );
+
+  @override
+  String toString() => 'TeamMembershipState($value)';
+}
