1e4924b858d96b18d028e67bbdd993db83f2500b
Abitofevrything <mylo.fawcett@gmail.com>
2023-06-18 18:22:28 +0200

    Handle adding an already existing member to a guild

--- lib/src/errors.dart
+++ lib/src/errors.dart
@@ -1,4 +1,5 @@
 import 'package:nyxx/src/gateway/shard.dart';
+import 'package:nyxx/src/models/snowflake.dart';
 
 /// The base class for all exceptions thrown by nyxx.
 class NyxxException implements Exception {
@@ -16,6 +17,18 @@ class NyxxException implements Exception {
 class InvalidEventException extends NyxxException {
   /// Create a new [InvalidEventException] with the provided [message].
   InvalidEventException(String message) : super('Invalid gateway event: $message');
+}
+
+/// An exception thrown when a member already exists in a guild.
+class MemberAlreadyExistsException extends NyxxException {
+  /// The ID of the guild.
+  final Snowflake guildId;
+
+  /// The ID of the member.
+  final Snowflake memberId;
+
+  /// Create a new [MemberAlreadyExistsException].
+  MemberAlreadyExistsException(this.guildId, this.memberId) : super('Member $memberId already exists in guild $guildId');
 }
 
 /// An error thrown when a shard disconnects unexpectedly.
