55a8c760b3a2cf0b481a3e7b941c9b9fe62cc359
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2023-11-06 14:12:57 +0100

    Fix autocompletion interaction responses (#585)

--- lib/src/builders/interaction_response.dart
+++ lib/src/builders/interaction_response.dart
@@ -49,7 +49,7 @@ class InteractionResponseBuilder extends CreateBuilder<InteractionResponseBuilde
 
   factory InteractionResponseBuilder.autocompleteResult(List<CommandOptionChoiceBuilder<dynamic>> choices) => InteractionResponseBuilder(
         type: InteractionCallbackType.applicationCommandAutocompleteResult,
-        data: choices,
+        data: {'choices': choices},
       );
 
   factory InteractionResponseBuilder.modal(ModalBuilder modal) => InteractionResponseBuilder(type: InteractionCallbackType.modal, data: modal);
