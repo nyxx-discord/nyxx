859941b30b7fa394b711093102753ad06ec071c1
Abitofevrything <mylo.fawcett@gmail.com>
2024-03-12 16:34:14 +0100

    Add builders for automoderation actions

--- lib/src/builders/guild/auto_moderation.dart
+++ lib/src/builders/guild/auto_moderation.dart
@@ -67,9 +67,9 @@ class AutoModerationRuleUpdateBuilder extends UpdateBuilder<AutoModerationRule> 
 
   AutoModerationEventType? eventType;
 
-  TriggerMetadata? metadata;
+  TriggerMetadataBuilder? metadata;
 
-  List<AutoModerationAction>? actions;
+  List<AutoModerationActionBuilder>? actions;
 
   bool? isEnabled;
 
@@ -91,30 +91,89 @@ class AutoModerationRuleUpdateBuilder extends UpdateBuilder<AutoModerationRule> 
   Map<String, Object?> build() => {
         if (name != null) 'name': name,
         if (eventType != null) 'event_type': eventType!.value,
-        if (metadata != null)
-          'trigger_metadata': {
-            'keyword_filter': metadata!.keywordFilter,
-            'regex_patterns': metadata!.regexPatterns,
-            'presets': metadata!.presets?.map((type) => type.value).toList(),
-            'allow_list': metadata!.allowList,
-            'mention_total_limit': metadata!.mentionTotalLimit,
-            'mention_raid_protection_enabled': metadata!.isMentionRaidProtectionEnabled,
-          },
-        if (actions != null)
-          'actions': [
-            for (final action in actions!)
-              {
-                'type': action.type.value,
-                if (action.metadata != null)
-                  'metadata': {
-                    'channel_id': action.metadata!.channelId?.toString(),
-                    'duration_seconds': action.metadata!.duration?.inSeconds,
-                    'custom_message': action.metadata!.customMessage,
-                  }
-              }
-          ],
+        if (metadata != null) 'trigger_metadata': metadata!.build(),
+        if (actions != null) 'actions': actions!.map((a) => a.build()).toList(),
         if (isEnabled != null) 'enabled': isEnabled,
         if (exemptRoleIds != null) 'exempt_roles': exemptRoleIds!.map((id) => id.toString()).toList(),
         if (exemptChannelIds != null) 'exempt_channels': exemptChannelIds!.map((id) => id.toString()).toList(),
+      };
+}
+
+class TriggerMetadataBuilder extends CreateBuilder<TriggerMetadata> {
+  /// A list of words that trigger the rule.
+  final List<String>? keywordFilter;
+
+  /// A list of regex patterns that trigger the rule.
+  final List<String>? regexPatterns;
+
+  /// A list of preset keyword types that trigger the rule.
+  final List<KeywordPresetType>? presets;
+
+  /// A list of words allowed to bypass the rule.
+  final List<String>? allowList;
+
+  /// The maximum number of mentions in a message.
+  final int? mentionTotalLimit;
+
+  /// Whether mention raid protection is enabled.
+  final bool? isMentionRaidProtectionEnabled;
+
+  TriggerMetadataBuilder({
+    required this.keywordFilter,
+    required this.regexPatterns,
+    required this.presets,
+    required this.allowList,
+    required this.mentionTotalLimit,
+    required this.isMentionRaidProtectionEnabled,
+  });
+
+  @override
+  Map<String, Object?> build() => {
+        'keyword_filter': keywordFilter,
+        'regex_patterns': regexPatterns,
+        'presets': presets?.map((type) => type.value).toList(),
+        'allow_list': allowList,
+        'mention_total_limit': mentionTotalLimit,
+        'mention_raid_protection_enabled': isMentionRaidProtectionEnabled,
+      };
+}
+
+class AutoModerationActionBuilder extends CreateBuilder<AutoModerationAction> {
+  /// The type of action to perform.
+  final ActionType type;
+
+  /// Metadata needed to perform the action.
+  final ActionMetadataBuilder? metadata;
+
+  AutoModerationActionBuilder({required this.type, required this.metadata});
+
+  @override
+  Map<String, Object?> build() => {
+        'type': type.value,
+        if (metadata != null) 'metadata': metadata!.build(),
+      };
+}
+
+class ActionMetadataBuilder extends CreateBuilder<ActionMetadata> {
+  /// The ID of the channel to send the alert message to.
+  final Snowflake? channelId;
+
+  /// The duration of time to time the user out for.
+  final Duration? duration;
+
+  /// A custom message to send to the user.
+  final String? customMessage;
+
+  ActionMetadataBuilder({
+    required this.channelId,
+    required this.duration,
+    required this.customMessage,
+  });
+
+  @override
+  Map<String, Object?> build() => {
+        'channel_id': channelId?.toString(),
+        'duration_seconds': duration?.inSeconds,
+        'custom_message': customMessage,
       };
 }
