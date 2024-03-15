d70f4a7f6b56a0b23bd3024957cddd9000c1c905
MCausc78 <mcausc78@gmail.com>
2024-03-15 19:39:55 +0200

    add banner to UserUpdateBuilder, and SkuFlags.available

--- lib/src/builders/user.dart
+++ lib/src/builders/user.dart
@@ -1,16 +1,24 @@
 import 'package:nyxx/src/builders/builder.dart';
 import 'package:nyxx/src/builders/image.dart';
+import 'package:nyxx/src/builders/sentinels.dart';
 import 'package:nyxx/src/models/user/user.dart';
 
 class UserUpdateBuilder extends UpdateBuilder<User> {
+  /// New user's username.
   String? username;
+
+  /// New user's avatar.
   ImageBuilder? avatar;
 
-  UserUpdateBuilder({this.username, this.avatar});
+  /// New user's banner.
+  ImageBuilder? banner;
+
+  UserUpdateBuilder({this.username, this.avatar = sentinelImageBuilder, this.banner = sentinelImageBuilder});
 
   @override
   Map<String, Object?> build() => {
         if (username != null) 'username': username!,
-        if (avatar != null) 'avatar': avatar!.buildDataString(),
+        if (!identical(avatar, sentinelImageBuilder)) 'avatar': avatar?.buildDataString(),
+        if (!identical(banner, sentinelImageBuilder)) 'banner': banner?.buildDataString(),
       };
 }
