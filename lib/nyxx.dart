2e6ef6d81e1481074c7140ebc1a409eead1d23c5
Rapougnac <me@rapougnac.moe>
2024-07-20 09:23:16 +0000

    export classes

--- lib/nyxx.dart
+++ lib/nyxx.dart
@@ -145,8 +145,9 @@ export 'src/models/message/attachment.dart' show Attachment, AttachmentFlags;
 export 'src/models/message/author.dart' show MessageAuthor;
 export 'src/models/message/channel_mention.dart' show ChannelMention;
 export 'src/models/message/embed.dart' show Embed, EmbedAuthor, EmbedField, EmbedFooter, EmbedImage, EmbedProvider, EmbedThumbnail, EmbedVideo;
-// ignore: deprecated_member_use_from_same_package
-export 'src/models/message/message.dart' show Message, MessageFlags, PartialMessage, MessageType, MessageInteraction, MessageInteractionMetadata;
+export 'src/models/message/message.dart'
+    // ignore: deprecated_member_use_from_same_package
+    show Message, MessageFlags, PartialMessage, MessageType, MessageInteraction, MessageInteractionMetadata, ForwardedMessage, MessageSnapshot;
 export 'src/models/message/poll.dart' show Poll, PollAnswer, PollAnswerCount, PollMedia, PollResults, PollLayoutType;
 export 'src/models/message/reaction.dart' show Reaction, ReactionCountDetails;
 export 'src/models/message/reference.dart' show MessageReference;
