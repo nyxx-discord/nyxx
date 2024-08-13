c275737675c1bba8425da0e287b4b19efa22d03e
Rapougnac <me@rapougnac.moe>
2024-08-13 10:35:26 +0200

    Remove import

--- lib/src/http/managers/role_manager.dart
+++ lib/src/http/managers/role_manager.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:nyxx/src/builders/role.dart';
-import 'package:nyxx/src/errors.dart';
 import 'package:nyxx/src/http/managers/manager.dart';
 import 'package:nyxx/src/http/request.dart';
 import 'package:nyxx/src/http/route.dart';
