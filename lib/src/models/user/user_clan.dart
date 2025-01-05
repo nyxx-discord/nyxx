64c80723fdfdcc36697831b63013332f434e29ae
Lexedia <contact@lexedia.moe>
2025-01-05 02:19:08 +0100

    revisions

--- lib/src/models/user/user_clan.dart
+++ lib/src/models/user/user_clan.dart
@@ -1,12 +1,12 @@
-import 'package:nyxx/src/client.dart';
 import 'package:nyxx/src/http/cdn/cdn_asset.dart';
+import 'package:nyxx/src/http/managers/user_manager.dart';
 import 'package:nyxx/src/http/route.dart';
 import 'package:nyxx/src/models/guild/guild.dart';
 import 'package:nyxx/src/models/snowflake.dart';
 import 'package:nyxx/src/utils/to_string_helper/to_string_helper.dart';
 
 class UserClan with ToStringHelper {
-  final NyxxRest _client;
+  final UserManager manager;
 
   /// The ID of this user's primary clan
   final Snowflake identityGuildId;
@@ -20,12 +20,11 @@ class UserClan with ToStringHelper {
   /// The clan badge hash.
   final String badgeHash;
 
-  UserClan({required this.identityGuildId, required this.badgeHash, required this.isIdentityEnabled, required this.tag, required NyxxRest client})
-      : _client = client;
+  UserClan({required this.identityGuildId, required this.badgeHash, required this.isIdentityEnabled, required this.tag, required this.manager});
 
   /// The guild of this user's primary clan
-  PartialGuild get identifyGuild => PartialGuild(id: identityGuildId, manager: _client.guilds);
+  PartialGuild get identifyGuild => manager.client.guilds[identityGuildId];
 
   /// The clan badge
-  CdnAsset get badge => CdnAsset(client: _client, base: HttpRoute()..clanBadges(id: identityGuildId.toString()), hash: badgeHash);
+  CdnAsset get badge => CdnAsset(client: manager.client, base: HttpRoute()..clanBadges(id: identityGuildId.toString()), hash: badgeHash);
 }
