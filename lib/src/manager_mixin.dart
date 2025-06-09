315ca6cf5072cbcf54e5ac67bc539e76604d5457
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2025-06-09 22:37:17 +0200

    Reduce the number of managers present in the process's memory (#753)

--- lib/src/manager_mixin.dart
+++ lib/src/manager_mixin.dart
@@ -19,38 +19,51 @@ mixin ManagerMixin implements Nyxx {
   RestClientOptions get options;
 
   /// A [UserManager] that manages users for this client.
-  UserManager get users => UserManager(options.userCacheConfig, this as NyxxRest);
+  UserManager get users => _users;
+  late final UserManager _users = UserManager(options.userCacheConfig, this as NyxxRest);
 
   /// A [ChannelManager] that manages channels for this client.
-  ChannelManager get channels => ChannelManager(options.channelCacheConfig, this as NyxxRest, stageInstanceConfig: options.stageInstanceCacheConfig);
+  ChannelManager get channels => _channels;
+  late final ChannelManager _channels = ChannelManager(options.channelCacheConfig, this as NyxxRest, stageInstanceConfig: options.stageInstanceCacheConfig);
 
   /// A [WebhookManager] that manages webhooks for this client.
-  WebhookManager get webhooks => WebhookManager(options.webhookCacheConfig, this as NyxxRest);
+  WebhookManager get webhooks => _webhooks;
+  late final WebhookManager _webhooks = WebhookManager(options.webhookCacheConfig, this as NyxxRest);
 
   /// A [GuildManager] that manages guilds for this client.
-  GuildManager get guilds => GuildManager(options.guildCacheConfig, this as NyxxRest);
+  GuildManager get guilds => _guilds;
+  late final GuildManager _guilds = GuildManager(options.guildCacheConfig, this as NyxxRest);
 
   /// An [ApplicationManager] that manages applications for this client.
-  ApplicationManager get applications => ApplicationManager(this as NyxxRest);
+  ApplicationManager get applications => _applications;
+  late final ApplicationManager _applications = ApplicationManager(this as NyxxRest);
 
   /// A [VoiceManager] that manages voice states for this client.
-  VoiceManager get voice => VoiceManager(this as NyxxRest);
+  VoiceManager get voice => _voice;
+  late final VoiceManager _voice = VoiceManager(this as NyxxRest);
 
   /// An [InviteManager] that manages invites for this client.
-  InviteManager get invites => InviteManager(this as NyxxRest);
+  InviteManager get invites => _invites;
+  late final InviteManager _invites = InviteManager(this as NyxxRest);
 
   /// A [GatewayManager] that manages gateway metadata for this client.
-  GatewayManager get gateway => GatewayManager(this as NyxxRest);
+  GatewayManager get gateway => _gateway;
+  late final GatewayManager _gateway = GatewayManager(this as NyxxRest);
 
   /// A [GlobalStickerManager] that manages global stickers.
-  GlobalStickerManager get stickers => GlobalStickerManager(options.globalStickerCacheConfig, this as NyxxRest);
+  GlobalStickerManager get stickers => _stickers;
+  late final GlobalStickerManager _stickers = GlobalStickerManager(options.globalStickerCacheConfig, this as NyxxRest);
 
   /// A [GlobalApplicationCommandManager] that manages global application commands.
-  GlobalApplicationCommandManager get commands =>
+  GlobalApplicationCommandManager get commands => _commands;
+  late final GlobalApplicationCommandManager _commands =
       GlobalApplicationCommandManager(options.applicationCommandConfig, this as NyxxRest, applicationId: (this as NyxxRest).application.id);
 
-  InteractionManager get interactions => InteractionManager(this as NyxxRest, applicationId: (this as NyxxRest).application.id);
+  /// An [InteractionManager] that manages interactions received by the client.
+  InteractionManager get interactions => _interactions;
+  late final InteractionManager _interactions = InteractionManager(this as NyxxRest, applicationId: (this as NyxxRest).application.id);
 
   /// A [GlobalSoundboardManager] that manages global soundboard sounds.
-  GlobalSoundboardManager get soundboard => GlobalSoundboardManager(options.globalSoundboardCacheConfig, this as NyxxRest);
+  GlobalSoundboardManager get soundboard => _soundboard;
+  late final GlobalSoundboardManager _soundboard = GlobalSoundboardManager(options.globalSoundboardCacheConfig, this as NyxxRest);
 }
